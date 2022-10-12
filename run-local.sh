89115ffeb72827ac10d2351ee8589e6776df09de
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-10-12 11:45:20 +0100

    SAA-108 running the service in dev with the spring DEV profile to only include seed data in that env.

--- run-local.sh
+++ run-local.sh
@@ -2,7 +2,7 @@
 # This script is used to run the Activities management API locally.
 #
 # It runs with a combination of properties from the default spring profile (in application.yaml) and supplemented
-# with the -dev profile (from application-dev.yml). The latter overrides some of the defaults.
+# with the -local profile (from application-local.yml). The latter overrides some of the defaults.
 #
 # The environment variables here will also override values supplied in spring profile properties, specifically
 # around removing the SSL connection to the database and setting the DB properties, SERVER_PORT and client credentials
@@ -16,8 +16,8 @@ export DB_USER=activities-management
 export DB_PASS=activities-management
 export DB_SSL_MODE=prefer
 
-# Run the application with stdout and dev profiles active
-SPRING_PROFILES_ACTIVE=stdout,dev ./gradlew bootRun
+# Run the application with stdout and local profiles active
+SPRING_PROFILES_ACTIVE=stdout,local ./gradlew bootRun
 
 # End
 
