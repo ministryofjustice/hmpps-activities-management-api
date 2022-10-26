af662cd7d6f8aed29078e813298e2815cd4a2d04
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2022-10-26 17:12:15 +0100

    SAA-129 english level 1 activity seed data. (#54)

---
+++ src/main/resources/migration/data/R__3_1__english_level_1_activity.sql
@@ -0,0 +1,2 @@
+insert into activity(activity_id, prison_code, activity_category_id, activity_tier_id, summary, description, start_date, end_date, active, created_time, created_by)
+values (2, 'MDI', 1, 1, 'English level 1', 'A basic english course suitable for introduction to the subject', '2022-10-10', null, true, '2022-10-10 09:00:00', 'SEED USER');
---
+++ src/main/resources/migration/data/R__3_2__english_level_1_activity_schedule.sql
@@ -0,0 +1,15 @@
+-- Tuesday AM
+insert into activity_schedule(activity_schedule_id, activity_id, description, suspend_until, start_time, end_time, internal_location_id, internal_location_code, internal_location_description, capacity, days_of_week)
+values (5, 2, 'Tuesday AM Houseblock 3', null, '10:00:00', '11:00:00', 1, 'EDU-ROOM-1', 'Education - R1', 10, '0100000');
+
+-- Tuesday PM
+insert into activity_schedule(activity_schedule_id, activity_id, description, suspend_until, start_time, end_time, internal_location_id, internal_location_code, internal_location_description, capacity, days_of_week)
+values (6, 2, 'Tuesday PM Houseblock 3', null, '14:00:00', '15:00:00', 2, 'EDU-ROOM-2', 'Education - R2', 10, '0100000');
+
+-- Thursday AM
+insert into activity_schedule(activity_schedule_id, activity_id, description, suspend_until, start_time, end_time, internal_location_id, internal_location_code, internal_location_description, capacity, days_of_week)
+values (7, 2, 'Thursday AM Houseblock 3', null, '10:00:00', '11:00:00', 1, 'EDU-ROOM-1', 'Education - R1', 10, '0001000');
+
+-- Thursday PM
+insert into activity_schedule(activity_schedule_id, activity_id, description, suspend_until, start_time, end_time, internal_location_id, internal_location_code, internal_location_description, capacity, days_of_week)
+values (8, 2, 'Thursday PM Houseblock 3', null, '14:00:00', '15:00:00', 2, 'EDU-ROOM-2', 'Education - R2', 10, '0001000');
