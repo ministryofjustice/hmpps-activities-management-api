726f7245980efc287292510b43feaa1d393cbf8c
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-11-23 16:13:27 +0000

    SAA-198 adding the domain model and DB DDL for event priorities.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/PrisonRegimeServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/PrisonRegimeServiceTest.kt
@@ -2,22 +2,57 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.EventPriority
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.EventType
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.EventPriorityRepository
 
 class PrisonRegimeServiceTest {
 
-  private val service = PrisonRegimeService()
+  private val eventPriorityRepository: EventPriorityRepository = mock()
+  private val service = PrisonRegimeService(eventPriorityRepository)
 
   @Test
-  fun `default priorities`() {
+  fun `default priorities are returned when no priorities for prison`() {
+    whenever(eventPriorityRepository.findByPrisonCode("PVI")).thenReturn(emptyList())
+
     assertThat(service.getEventPrioritiesForPrison("PVI")).containsExactlyInAnyOrderEntriesOf(
       mapOf(
-        EventType.COURT_HEARING to setOf(Priority(1)),
-        EventType.VISIT to setOf(Priority(2)),
-        EventType.ADJUDICATION_HEARING to setOf(Priority(3)),
-        EventType.APPOINTMENT to setOf(Priority(4)),
-        EventType.ACTIVITY to setOf(Priority(5)),
+        EventType.COURT_HEARING to listOf(Priority(1)),
+        EventType.VISIT to listOf(Priority(2)),
+        EventType.ADJUDICATION_HEARING to listOf(Priority(3)),
+        EventType.APPOINTMENT to listOf(Priority(4)),
+        EventType.ACTIVITY to listOf(Priority(5)),
       )
     )
+
+    verify(eventPriorityRepository).findByPrisonCode("PVI")
+  }
+
+  @Test
+  fun `existing prison priorities are returned for prison, overriding defaults`() {
+    whenever(eventPriorityRepository.findByPrisonCode("MDI")).thenReturn(
+      listOf(
+        EventPriority(1, "MDI", EventType.ACTIVITY, 1),
+        EventPriority(2, "MDI", EventType.APPOINTMENT, 2),
+        EventPriority(3, "MDI", EventType.VISIT, 3),
+        EventPriority(4, "MDI", EventType.ADJUDICATION_HEARING, 4),
+        EventPriority(5, "MDI", EventType.COURT_HEARING, 5),
+      )
+    )
+
+    assertThat(service.getEventPrioritiesForPrison("MDI")).containsExactlyInAnyOrderEntriesOf(
+      mapOf(
+        EventType.ACTIVITY to listOf(Priority(1)),
+        EventType.APPOINTMENT to listOf(Priority(2)),
+        EventType.VISIT to listOf(Priority(3)),
+        EventType.ADJUDICATION_HEARING to listOf(Priority(4)),
+        EventType.COURT_HEARING to listOf(Priority(5)),
+      )
+    )
+
+    verify(eventPriorityRepository).findByPrisonCode("MDI")
   }
 }
