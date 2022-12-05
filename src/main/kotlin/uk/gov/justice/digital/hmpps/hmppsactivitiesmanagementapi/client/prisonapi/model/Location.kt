69e2e008f7349c01abaa6a9c5dd4dcd7804f299f
Mark Ford <mark.ford@digital.justice.gov.uk>
2022-12-05 14:20:31 +0000

    SAA-221 Create Activities Management API location endpoints
    swagger endpoint improvement

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/client/prisonapi/model/Location.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/client/prisonapi/model/Location.kt
@@ -19,39 +19,39 @@ import io.swagger.v3.oas.annotations.media.Schema
  */
 data class Location(
 
-  @Schema(example = "null", required = true, description = "Location identifier.")
+  @Schema(example = "721705", required = true, description = "Location identifier.")
   @JsonProperty("locationId", required = true) val locationId: Long,
 
-  @Schema(example = "null", required = true, description = "Location type.")
+  @Schema(example = "CELL", required = true, description = "Location type.")
   @JsonProperty("locationType", required = true) val locationType: String,
 
-  @Schema(example = "null", required = true, description = "Location description.")
+  @Schema(example = "MDI-RES-HB1-ALE", required = true, description = "Location description.")
   @JsonProperty("description", required = true) val description: String,
 
-  @Schema(example = "null", required = true, description = "Identifier of Agency this location is associated with.")
+  @Schema(example = "MDI", required = true, description = "Identifier of Agency this location is associated with.")
   @JsonProperty("agencyId", required = true) val agencyId: String,
 
-  @Schema(example = "null", description = "What events this room can be used for.")
+  @Schema(example = "APP", description = "What events this room can be used for.")
   @JsonProperty("locationUsage") val locationUsage: String? = null,
 
-  @Schema(example = "null", description = "Identifier of this location's parent location.")
+  @Schema(example = "26960", description = "Identifier of this location's parent location.")
   @JsonProperty("parentLocationId") val parentLocationId: Long? = null,
 
-  @Schema(example = "null", description = "Current occupancy of location.")
+  @Schema(example = "10", description = "Current occupancy of location.")
   @JsonProperty("currentOccupancy") val currentOccupancy: Int? = null,
 
   @Schema(
-    example = "null",
+    example = "RES-HB1-ALE",
     description = "Location prefix. Defines search prefix that will constrain search to this location and its subordinate locations."
   )
   @JsonProperty("locationPrefix") val locationPrefix: String? = null,
 
-  @Schema(example = "null", description = "Operational capacity of the location.")
+  @Schema(example = "20", description = "Operational capacity of the location.")
   @JsonProperty("operationalCapacity") val operationalCapacity: Int? = null,
 
-  @Schema(example = "null", description = "User-friendly location description.")
+  @Schema(example = "RES-HB1-ALE", description = "User-friendly location description.")
   @JsonProperty("userDescription") val userDescription: String? = null,
 
-  @Schema(example = "null", description = "")
+  @Schema(example = "", description = "Internal location code")
   @JsonProperty("internalLocationCode") val internalLocationCode: String? = null
 )
