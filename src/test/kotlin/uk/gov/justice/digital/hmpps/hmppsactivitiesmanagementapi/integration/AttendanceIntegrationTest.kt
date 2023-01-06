b19bef0e58e6446c0d14787843a2f91db8ac19c6
Mark Ford <mark.ford@digital.justice.gov.uk>
2023-01-06 12:02:54 +0000

    SAA-281 Roles
    Integration test for attendances

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/AttendanceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/AttendanceIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AttendanceUpdateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AttendanceRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Attendance as EntityAttendance
@@ -45,13 +46,46 @@ class AttendanceIntegrationTest : IntegrationTestBase() {
         )
       )
       .accept(MediaType.APPLICATION_JSON)
-      .headers(setAuthorisation(roles = listOf()))
+      .headers(setAuthorisation(roles = listOf("ROLE_ACTIVITY_ADMIN")))
       .exchange()
       .expectStatus().isNoContent
 
     val markedAttendances = attendanceRepository.findAll().toList().also { assertThat(it).hasSize(2) }
     assertThat(markedAttendances.prisonerAttendanceReason("A11111A").code).isEqualTo("ATT")
     assertThat(markedAttendances.prisonerAttendanceReason("A22222A").code).isEqualTo("ABS")
+  }
+
+  @Sql(
+    "classpath:test_data/seed-activity-id-1.sql"
+  )
+  @Test
+  fun `403 error morning attendances are marked without the correct role`() {
+    val unmarkedAttendances = attendanceRepository.findAll().also { assertThat(it).hasSize(2) }
+    unmarkedAttendances.forEach { assertThat(it.attendanceReason).isNull() }
+
+    val error = webTestClient.put()
+      .uri("/attendances")
+      .bodyValue(
+        listOf(
+          AttendanceUpdateRequest(1, "ATT"),
+          AttendanceUpdateRequest(2, "ABS")
+        )
+      )
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_NOT_ALLOWED")))
+      .exchange()
+      .expectStatus().isForbidden
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(ErrorResponse::class.java)
+      .returnResult().responseBody
+
+    with(error!!) {
+      assertThat(status).isEqualTo(403)
+      assertThat(errorCode).isNull()
+      assertThat(userMessage).isEqualTo("Access denied: Access is denied")
+      assertThat(developerMessage).isEqualTo("Access is denied")
+      assertThat(moreInfo).isNull()
+    }
   }
 
   private fun WebTestClient.getAttendancesForInstance(instanceId: Long) =
