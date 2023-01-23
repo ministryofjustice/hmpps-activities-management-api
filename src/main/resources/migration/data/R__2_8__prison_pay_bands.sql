3c8a063bc2f2ad240740cf7ed0cfdd0f66d654b3
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-01-23 17:36:22 +0000

    SAA-311 adding pay band aliases for Moorland.

--- src/main/resources/migration/data/R__2_8__prison_pay_bands.sql
+++ src/main/resources/migration/data/R__2_8__prison_pay_bands.sql
@@ -8,6 +8,9 @@ values (1, 1, 1, 'Pay band 1 (lowest)', 'Pay band 1 (lowest)', 'DEFAULT'),
        (7, 7, 7, 'Pay band 7', 'Pay band 7', 'DEFAULT'),
        (8, 8, 8, 'Pay band 8', 'Pay band 8', 'DEFAULT'),
        (9, 9, 9, 'Pay band 9', 'Pay band 9', 'DEFAULT'),
-       (10, 10, 10, 'Pay band 10 (highest)', 'Pay band 10 (highest)', 'DEFAULT');
+       (10, 10, 10, 'Pay band 10 (highest)', 'Pay band 10 (highest)', 'DEFAULT'),
+       (11, 1, 1, 'Low', 'Pay band 1', 'MDI'),
+       (12, 2, 2, 'Medium', 'Pay band 2', 'MDI'),
+       (13, 3, 3, 'High', 'Pay band 3', 'MDI');
 
-alter sequence event_priority_event_priority_id_seq restart with 11;
+alter sequence event_priority_event_priority_id_seq restart with 14;
