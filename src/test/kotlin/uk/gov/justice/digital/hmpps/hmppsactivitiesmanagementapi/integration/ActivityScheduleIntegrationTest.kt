ffe3bb2662ce1e18bd6e7d1c14f43cdd1e03a6d5
Gerry Allan <gerry.allan@digital.justice.gov.uk>
2023-01-24 22:45:14 +0000

    SAA-296: Integration test updates for Spring Boot 3 upgrade

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ActivityScheduleIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ActivityScheduleIntegrationTest.kt
@@ -157,8 +157,8 @@ class ActivityScheduleIntegrationTest : IntegrationTestBase() {
     with(error!!) {
       assertThat(status).isEqualTo(403)
       assertThat(errorCode).isNull()
-      assertThat(userMessage).isEqualTo("Access denied: Access is denied")
-      assertThat(developerMessage).isEqualTo("Access is denied")
+      assertThat(userMessage).isEqualTo("Access denied: Access Denied")
+      assertThat(developerMessage).isEqualTo("Access Denied")
       assertThat(moreInfo).isNull()
     }
   }
