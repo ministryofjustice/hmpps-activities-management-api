d740d9f0cf6f6189ab8dabfc68cef04e3308051e
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-02-06 12:05:34 +0000

    SAA-326: add DDL and seed data

--- src/main/resources/migration/data/R__2_9__prison_regime.sql
+++ src/main/resources/migration/data/R__2_9__prison_regime.sql
@@ -1,5 +1,5 @@
 insert into prison_regime
 (prison_code, am_start, am_finish, pm_start, pm_finish, ed_start, ed_finish)
-VALUES(1, 'MDI', '09:00:00', '12:00:00', '13:00:00', '16:30:00', '18:00:00', '20:00:00');
+VALUES('MDI', '09:00:00', '12:00:00', '13:00:00', '16:30:00', '18:00:00', '20:00:00');
 
 alter sequence prison_regime_prison_regime_id_seq restart with 2;
