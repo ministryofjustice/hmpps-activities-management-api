ae61f79f526ab72ce99d4020426154e34d3e9253
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-02-08 16:09:03 +0000

    SAA-348: Create appointment API endpoint stub

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/AppointmentController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/AppointmentController.kt
@@ -1,19 +1,27 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.resource
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.Appointment
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentCreateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.AppointmentService
+import java.security.Principal
 
 @RestController
 @RequestMapping("/appointments", produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -62,4 +70,57 @@ class AppointmentController(
   )
   fun getAppointmentById(@PathVariable("appointmentId") appointmentId: Long): Appointment =
     appointmentService.getAppointmentById(appointmentId)
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  @Operation(
+    summary = "Create an appointment or series of appointment occurrences",
+    description =
+    """
+    Create an appointment or series of appointment occurrences and allocate the supplied prisoner or prisoners to them.
+    Does not require any specific roles
+    """
+  )
+
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "201",
+        description = "The appointment or series of appointment occurrences was created.",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = Appointment::class)
+          )
+        ],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class)
+          )
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class)
+          )
+        ],
+      ),
+    ]
+  )
+  fun createAppointment(
+    principal: Principal,
+    @Valid @RequestBody @Parameter(
+      description = "The create request with the new appointment or series of appointment occurrences details",
+      required = true
+    ) appointmentCreateRequest: AppointmentCreateRequest
+  ): Appointment = appointmentService.createAppointment(appointmentCreateRequest)
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AppointmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AppointmentService.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.Appointment
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentCreateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.findOrThrowNotFound
 
@@ -10,4 +12,8 @@ class AppointmentService(
 ) {
   fun getAppointmentById(appointmentId: Long) =
     appointmentRepository.findOrThrowNotFound(appointmentId).toModel()
+
+  fun createAppointment(appointmentCreateRequest: AppointmentCreateRequest): Appointment {
+    TODO("Not yet implemented")
+  }
 }
