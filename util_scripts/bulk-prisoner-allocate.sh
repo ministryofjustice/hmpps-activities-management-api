bfb861dfaaf617fc2f5a69b2edea8849d5d6d6ae
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-02-21 10:27:53 +0000

    SAA-365 minor refactorings of bash util script for bulk allocation of offenders.

--- util_scripts/bulk-prisoner-allocate.sh
+++ util_scripts/bulk-prisoner-allocate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-
+#
+# Script for bulk prisoner allocations using direct API calls by parsing the bulk-prisoner-allocations.csv file
+# containing the schedule ids, prisoner numbers and their pay bands.
+#
+# File format is the schedule id, prisoner number and then pay band, for example:
+#
+#   1,A1234AA,1
+#   1,A3444BB,2
+#   ...
+#
+# Note: please ensure there is a carriage return on the last line of the CSV file!!
+#
+# Upon running you will be prompted to supply the environment (dev/preprod/prod) for the bulk allocations.
+#
 read -r -p "Please enter the environment for the bulk allocations dev/preprod/prod: " ENVIRONMENT
 
 function getHosts() {
@@ -10,13 +23,10 @@ function getHosts() {
     AUTH_HOST="https://sign-in-$ENVIRONMENT.hmpps.service.justice.gov.uk"
     API_HOST="https://activities-api-$ENVIRONMENT.prison.service.justice.gov.uk"
   fi
-
- echo Using Auth host $AUTH_HOST
- echo Useing API host $API_HOST
 }
 
 function getCsvFile() {
-  read -r -p "Please enter the batch allocations CSV filename: " BATCH_ALLOCATIONS_CSV_FILE
+  BATCH_ALLOCATIONS_CSV_FILE=bulk-prisoner-allocations.csv
 
   [ ! -f "$BATCH_ALLOCATIONS_CSV_FILE" ] && {
     echo "$BATCH_ALLOCATIONS_CSV_FILE file not found"
@@ -30,49 +40,35 @@ function getBearerToken() {
   TOKEN=$(curl -s -X POST "$AUTH_HOST/auth/oauth/token?grant_type=client_credentials" -H "Content-Length:0" --user "$CLIENT_ID":"$CLIENT_SECRET" | jq -r '.access_token')
 }
 
-function getSchedule() {
-  read -r -p "Please enter the schedule ID: " SCHEDULE_ID
-
-  STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" "$API_HOST/schedules/$SCHEDULE_ID" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN")
-
-  if [ "$STATUS" != "200" ]; then
-    echo HTTP STATUS CODE "$STATUS" - error reading schedule ID "${SCHEDULE_ID}"
-    exit 1
-  fi
-}
-
 function allocatePrisoners() {
-  while IFS="," read -r prison_number pay_band_id; do
-    curl \
-      -s \
-      -o /dev/null \
-      -H "Content-Type: application/json" \
-      -H "Authorization: Bearer $TOKEN" \
+  read -r -p "Please confirm you wish to bulk allocate prisoners in $ENVIRONMENT in the bulk-prisoner-allocations.csv file? (yes/no) " yn
+
+  case $yn in
+  yes) ;;
+  no)
+    echo Bulk allocations cancelled!
+    exit
+    ;;
+  *)
+    echo Please respond with yes or no.
+    exit 1
+    ;;
+  esac
+
+  while IFS="," read -r schedule_id prison_number pay_band_id; do
+    curl -s -o /dev/null \
+      -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" \
       -d '{ "prisonerNumber": "'"${prison_number}"'", "payBandId": '"${pay_band_id}"' }' \
-      POST "$API_HOST/schedules/$SCHEDULE_ID/allocations"
+      POST "$API_HOST/schedules/${schedule_id}/allocations"
     echo ""
+
   done <"$BATCH_ALLOCATIONS_CSV_FILE"
 }
 
 getHosts
 getCsvFile
 getBearerToken
-getSchedule
-
-read -r -p "Please confirm you wish to allocate prisoners to schedule ID $SCHEDULE_ID in $ENVIRONMENT? (yes/no) " yn
-
-case $yn in
-yes) ;;
-no)
-  echo exiting...
-  exit
-  ;;
-*)
-  echo invalid response
-  exit 1
-  ;;
-esac
-
 allocatePrisoners
 
-echo Done!
+echo
+echo Bulk allocation of prisoners is complete
