6330ee0e63dcb85af61966b79d490829af9202f5
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-02-27 10:50:31 +0000

    SAA-401 adding new seeded activity category to support allocating prisoners to unemployed activities e.g. retired.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/testdata/IntegrationTestActivityCategories.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/testdata/IntegrationTestActivityCategories.kt
@@ -9,3 +9,4 @@ val gymSportsFitnessCategory = ActivityCategory(id = 4, code = "SAA_GYM_SPORTS_F
 val inductionCategory = ActivityCategory(id = 5, code = "SAA_INDUCTION", name = "Induction", description = "Such as gym induction, education assessments, health and safety workshops")
 val interventionsCategory = ActivityCategory(id = 6, code = "SAA_INTERVENTIONS", name = "Intervention programmes", description = "Such as programmes for behaviour management, drug and alcohol misuse and community rehabilitation")
 val leisureAndSocialCategory = ActivityCategory(id = 7, code = "SAA_LEISURE_SOCIAL", name = "Leisure and social", description = "Such as association, library time and social clubs, like music or art")
+val notInWorkCategory = ActivityCategory(id = 8, code = "SAA_UNEMPLOYMENT", name = "Not in work", description = "Such as unemployed, retired, long-term sick, or on remand")
