c6554823c32563bb20880754d2b2c108614c7f2d
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2023-02-27 13:40:44 +0000

    SAA-401 adding new seeded activity category to support allocating prisoners to unemployed activities e.g. retired. (#196)

--- src/main/resources/migration/data/R__1_2__activity_categories.sql
+++ src/main/resources/migration/data/R__1_2__activity_categories.sql
@@ -5,6 +5,7 @@ values (1, 'SAA_EDUCATION', 'Education', 'Such as classes in English, maths, con
        (4, 'SAA_GYM_SPORTS_FITNESS', 'Gym, sport and fitness', 'Such as sport clubs, like football, fitness classes and gym sessions'),
        (5, 'SAA_INDUCTION', 'Induction', 'Such as gym induction, education assessments, health and safety workshops'),
        (6, 'SAA_INTERVENTIONS', 'Intervention programmes', 'Such as programmes for behaviour management, drug and alcohol misuse and community rehabilitation'),
-       (7, 'SAA_LEISURE_SOCIAL', 'Leisure and social', 'Such as association, library time and social clubs, like music or art');
+       (7, 'SAA_LEISURE_SOCIAL', 'Leisure and social', 'Such as association, library time and social clubs, like music or art'),
+       (8, 'SAA_UNEMPLOYMENT', 'Not in work', 'Such as unemployed, retired, long-term sick, or on remand');
 
-alter sequence activity_category_activity_category_id_seq restart with 8;
+alter sequence activity_category_activity_category_id_seq restart with 9;
