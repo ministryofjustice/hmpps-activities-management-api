6243035685ed8ff9a32acf369921d5e9d24a45f2
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-03-07 17:53:45 +0000

    SAA-417 adding concept of deallocation to Allocation entity.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AllocationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AllocationTest.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.allocation
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.lowPayBand
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.mediumPayBand
 import java.time.LocalDate
@@ -46,5 +48,52 @@ class AllocationTest {
       assertThat(isActive(tomorrow.plusDays(1))).isFalse
       assertThat(payBand.payBandAlias).isEqualTo("Medium")
     }
+  }
+
+  @Test
+  fun `check allocation ends`() {
+    with(allocation().copy(endDate = today)) {
+      assertThat(ends(yesterday)).isFalse
+      assertThat(ends(today)).isTrue
+      assertThat(ends(tomorrow)).isFalse
+    }
+
+    with(allocation().copy(endDate = tomorrow)) {
+      assertThat(ends(yesterday)).isFalse
+      assertThat(ends(today)).isFalse
+      assertThat(ends(tomorrow)).isTrue
+    }
+
+    with(allocation().copy(endDate = null)) {
+      assertThat(ends(yesterday)).isFalse
+      assertThat(ends(today)).isFalse
+      assertThat(ends(tomorrow)).isFalse
+    }
+  }
+
+  @Test
+  fun `check can deallocate active allocation`() {
+    val dateTime = LocalDateTime.now()
+
+    assertThat(allocationWithEndDate.status(PrisonerStatus.ACTIVE)).isTrue
+    assertThat(allocationWithEndDate.deallocatedReason).isNull()
+    assertThat(allocationWithEndDate.deallocatedBy).isNull()
+    assertThat(allocationWithEndDate.deallocatedTime).isNull()
+
+    allocationWithEndDate.deallocate(dateTime)
+
+    assertThat(allocationWithEndDate.status(PrisonerStatus.ENDED)).isTrue
+    assertThat(allocationWithEndDate.deallocatedReason).isEqualTo("Allocation end date reached")
+    assertThat(allocationWithEndDate.deallocatedBy).isEqualTo("SYSTEM")
+    assertThat(allocationWithEndDate.deallocatedTime).isEqualTo(dateTime)
+  }
+
+  @Test
+  fun `check cannot deallocate if allocation already ended`() {
+    allocationWithEndDate.deallocate(LocalDateTime.now())
+
+    assertThatThrownBy { allocationWithEndDate.deallocate(LocalDateTime.now()) }
+      .isInstanceOf(IllegalStateException::class.java)
+      .hasMessage("Allocation with ID '-1' is already deallocated.")
   }
 }
