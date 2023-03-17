b70530bfd441a9adb79a15e4319da9b45180c0fc
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-03-17 15:18:53 +0000

    SAA-483: Bug fix monthly repeat last day of month (#224)
    * SAA-483: Test highlighting bug
    
    * SAA-483: Fix for bug
    
    * SAA-412: Resolving code review comments from previous PR
    
    * SAA-483: Rename test class
    
    * SAA-483: Date rename

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentRepeatPeriodTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentRepeatPeriodTest.kt
@@ -6,47 +6,47 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 
 class AppointmentRepeatPeriodTest {
-  val monday: LocalDate = LocalDate.of(2023, 3, 13)
-  val friday: LocalDate = LocalDate.of(2023, 3, 17)
+  private val monday: LocalDate = LocalDate.of(2023, 3, 13)
+  private val friday: LocalDate = LocalDate.of(2023, 3, 17)
 
   @Test
   fun `weekday next date increments by one day`() {
-    val nextDate = AppointmentRepeatPeriod.WEEKDAY.nextDate(monday)
+    val nextDate = AppointmentRepeatPeriod.WEEKDAY.occurrenceDate(monday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(14)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.TUESDAY)
   }
 
   @Test
   fun `weekday next date increments friday to monday`() {
-    val nextDate = AppointmentRepeatPeriod.WEEKDAY.nextDate(friday)
+    val nextDate = AppointmentRepeatPeriod.WEEKDAY.occurrenceDate(friday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(20)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.MONDAY)
   }
 
   @Test
   fun `daily next date increments by one day`() {
-    val nextDate = AppointmentRepeatPeriod.DAILY.nextDate(friday)
+    val nextDate = AppointmentRepeatPeriod.DAILY.occurrenceDate(friday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(18)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.SATURDAY)
   }
 
   @Test
   fun `weekly next date increments by one week`() {
-    val nextDate = AppointmentRepeatPeriod.WEEKLY.nextDate(monday)
+    val nextDate = AppointmentRepeatPeriod.WEEKLY.occurrenceDate(monday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(20)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.MONDAY)
   }
 
   @Test
   fun `fortnightly next date increments by two weeks`() {
-    val nextDate = AppointmentRepeatPeriod.FORTNIGHTLY.nextDate(monday)
+    val nextDate = AppointmentRepeatPeriod.FORTNIGHTLY.occurrenceDate(monday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(27)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.MONDAY)
   }
 
   @Test
-  fun `monthly next date increments by two weeks`() {
-    val nextDate = AppointmentRepeatPeriod.MONTHLY.nextDate(monday)
+  fun `monthly next date increments by one month`() {
+    val nextDate = AppointmentRepeatPeriod.MONTHLY.occurrenceDate(monday, 2)
     assertThat(nextDate.dayOfMonth).isEqualTo(13)
     assertThat(nextDate.dayOfWeek).isEqualTo(DayOfWeek.THURSDAY)
   }
