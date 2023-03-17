baeef7e71007112325c39f7314cf1ac4d402a95d
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-03-17 14:14:21 +0000

    SAA-483: Fix for bug

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentSchedule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentSchedule.kt
@@ -58,15 +58,12 @@ class AppointmentScheduleIterator(
   private val repeatPeriod: AppointmentRepeatPeriod,
   private val repeatCount: Int,
 ) : Iterator<LocalDate> {
-  private var date = startDate
   private var count = 0
 
   override fun hasNext() = count < repeatCount
 
   override fun next(): LocalDate {
-    val next = date
-    date = repeatPeriod.nextDate(date)
     count++
-    return next
+    return repeatPeriod.occurrenceDate(startDate, count)
   }
 }
