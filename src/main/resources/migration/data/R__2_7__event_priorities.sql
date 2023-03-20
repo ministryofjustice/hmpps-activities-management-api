30269fb825947b6368be28c99e4e381ae34d92bd
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2023-03-20 09:45:33 +0000

    SAA-247 fixing index violation on the event priorities. (#225)

--- src/main/resources/migration/data/R__2_7__event_priorities.sql
+++ src/main/resources/migration/data/R__2_7__event_priorities.sql
@@ -2,7 +2,7 @@ insert into event_priority (event_priority_id, prison_code, event_type, event_ca
 values (1, 'MDI', 'COURT_HEARING', null, 1),
        (2, 'MDI', 'EXTERNAL_TRANSFER', null, 2),
        (3, 'MDI', 'ADJUDICATION_HEARING', null, 3),
-       (5, 'MDI', 'VISIT', null, 4),
+       (4, 'MDI', 'VISIT', null, 4),
        (5, 'MDI', 'APPOINTMENT', null, 5),
        (6, 'MDI', 'ACTIVITY', null, 6);
 
