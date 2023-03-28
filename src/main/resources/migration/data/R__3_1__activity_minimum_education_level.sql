7c81e1da598cfbaeef9a86c86176642d005bd5ad
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-03-28 14:27:12 +0100

    SAA-407: Refactor migration scripts to allow for ref data to be loaded into different environments

--- src/main/resources/migration/data/R__3_1__activity_minimum_education_level.sql
+++
@@ -1,5 +0,0 @@
-insert into activity_minimum_education_level(activity_minimum_education_level_id, activity_id, education_level_code, education_level_description)
-values (1, 1, '1', 'Reading Measure 1.0'),
-       (2, 1, '1.1', 'Reading Measure 1.1');
-
-alter sequence activity_minimum_education_le_activity_minimum_education_le_seq restart with 3;
