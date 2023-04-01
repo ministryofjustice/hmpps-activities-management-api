fd2453c98316fec7ad81393b9d629f6511300153
Simon Oldham <simon.oldham@gdsgroup.com>
2023-04-01 22:26:28 +0100

    Adding documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/AuditRecordSearchFilters.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/AuditRecordSearchFilters.kt
@@ -1,17 +1,37 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.audit.AuditEventType
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.audit.AuditType
 import java.time.LocalDateTime
 
+@Schema(description = "Set of search filters for searching audit records")
 data class AuditRecordSearchFilters(
+
+  @Schema(description = "The code of the prison", example = "PVI")
   val prisonCode: String? = null,
+
+  @Schema(description = "The prisoner number", example = "A1234AA")
   val prisonerNumber: String? = null,
+
+  @Schema(description = "The username of the logged-in user", example = "JONESA")
   val username: String? = null,
+
+  @Schema(description = "The top-level audit category", example = "ACTIVITY")
   val auditType: AuditType? = null,
+
+  @Schema(description = "The specific event type", example = "ACTIVITY_CREATED")
   val auditEventType: AuditEventType? = null,
+
+  @Schema(description = "The date and time on or after which to search for events", example = "2022-09-01T09:01:02")
   val startTime: LocalDateTime? = null,
+
+  @Schema(description = "The date and time on or before which to search for events", example = "2022-09-01T09:01:02")
   val endTime: LocalDateTime? = null,
+
+  @Schema(description = "The ID of the activity", example = "123456")
   val activityId: Long? = null,
+
+  @Schema(description = "The ID of the activity schedule", example = "123456")
   val scheduleId: Long? = null,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/response/LocalAuditSearchResults.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/response/LocalAuditSearchResults.kt
@@ -1,9 +1,17 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.LocalAuditRecord
 
+@Schema(description = "The result of an audit record search")
 data class LocalAuditSearchResults(
+
+  @Schema(description = "The matching records")
   val content: List<LocalAuditRecord>,
+
+  @Schema(description = "The current page number", example = "0")
   val pageNumber: Int,
+
+  @Schema(description = "The total number of pages", example = "5")
   val totalPages: Int,
 )
