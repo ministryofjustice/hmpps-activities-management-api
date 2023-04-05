4136fcffeb439f0369c0b662da06d3b80d5f36b2
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-04-05 14:25:28 +0100

    SAA-502: add previous and next IDs to the ScheduledInstance

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/ActivityScheduleTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/ActivityScheduleTest.kt
@@ -489,4 +489,35 @@ class ActivityScheduleTest {
 
     assertThat(scheduleWithSlot.instances()).isEmpty()
   }
+
+  @Test
+  fun `can retrieve previous and next instances where available`() {
+    val scheduleWithInstances = ActivitySchedule(
+      activity = activityEntity(),
+      description = "description",
+      capacity = 1,
+      startDate = today,
+      internalLocationId = 1,
+      internalLocationCode = "Loc Code",
+      internalLocationDescription = "Loc Code Desc",
+    ).apply {
+      addSlot(
+        startTime = today.atStartOfDay().toLocalTime(),
+        endTime = today.atStartOfDay().toLocalTime().plusHours(1),
+        setOf(DayOfWeek.MONDAY),
+      )
+    }
+
+    scheduleWithInstances.addInstance(today, scheduleWithInstances.slots().first())
+    scheduleWithInstances.addInstance(today.plusDays(1L), scheduleWithInstances.slots().first())
+    scheduleWithInstances.addInstance(today.plusDays(2L), scheduleWithInstances.slots().first())
+
+    assertThat(scheduleWithInstances.instances()).hasSize(3)
+
+    assertThat(scheduleWithInstances.previous(scheduleWithInstances.instances().first())?.toModel()?.previousScheduledInstanceDate).isNull()
+    assertThat(scheduleWithInstances.next(scheduleWithInstances.instances().first())?.toModel()?.nextScheduledInstanceDate).isNotNull()
+
+    assertThat(scheduleWithInstances.previous(scheduleWithInstances.instances().last())?.toModel()?.previousScheduledInstanceDate).isNotNull()
+    assertThat(scheduleWithInstances.next(scheduleWithInstances.instances().last())?.toModel()?.nextScheduledInstanceDate).isNull()
+  }
 }
