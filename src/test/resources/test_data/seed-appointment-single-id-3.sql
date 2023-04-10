d7a32ec355b881abbeea3b1defa548ef418385a0
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2023-04-10 12:24:17 +0100

    SAA-547: Updated integration test seed data to include appointment type.

--- src/test/resources/test_data/seed-appointment-single-id-3.sql
+++ src/test/resources/test_data/seed-appointment-single-id-3.sql
@@ -1,5 +1,5 @@
-INSERT INTO appointment (appointment_id, category_code, prison_code, internal_location_id, in_cell, start_date, start_time, end_time, comment, created, created_by)
-VALUES (3, 'AC1', 'MDI', 123, false, '2022-10-01', '09:00', '10:30', 'Appointment level comment', now()::timestamp, 'TEST.USER');
+INSERT INTO appointment (appointment_id, appointment_type, category_code, prison_code, internal_location_id, in_cell, start_date, start_time, end_time, comment, created, created_by)
+VALUES (3, 'INDIVIDUAL', 'AC1', 'MDI', 123, false, '2022-10-01', '09:00', '10:30', 'Appointment level comment', now()::timestamp, 'TEST.USER');
 
 INSERT INTO appointment_occurrence (appointment_occurrence_id, appointment_id, sequence_number, internal_location_id, in_cell, start_date, start_time, end_time, comment)
 VALUES (3, 3, 1, 123, false, '2022-10-01', '09:00', '10:30', 'Appointment occurrence level comment');
