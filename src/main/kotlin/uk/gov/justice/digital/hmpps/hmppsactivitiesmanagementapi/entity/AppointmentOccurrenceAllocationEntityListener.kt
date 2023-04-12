bbda6fb577f11bdc0eb7aca41c45354b44dc4904
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-04-12 18:05:32 +0100

    SAA-485: Added comments explaining the use of allocation id for instance id

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentOccurrenceAllocationEntityListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentOccurrenceAllocationEntityListener.kt
@@ -23,8 +23,9 @@ class AppointmentOccurrenceAllocationEntityListener {
   /**
    * Appointment instances are provided by a view that joins allocations, occurrences and appointments to create
    * a per prisoner per date individual appointment instance. This resulting appointment instance uses the allocation id
-   * as there is a one-to-one relationship between allocations and instances. Appointment instances are compatible with
-   * NOMIS appointments stored in the OFFENDER_IND_SCHEDULES table. This event is used primarily for sync purposes.
+   * as there is a one-to-one relationship between allocations and instances. This is therefore the id that should be
+   * used for the /appointment-instances/:id endpoint. Appointment instances are compatible with NOMIS appointments
+   * stored in the OFFENDER_IND_SCHEDULES table. This event and associated endpoint are used primarily for sync purposes.
    */
   @PostPersist
   fun onCreate(entity: AppointmentOccurrenceAllocation) {
@@ -38,6 +39,13 @@ class AppointmentOccurrenceAllocationEntityListener {
     }
   }
 
+  /**
+   * Appointment instances are provided by a view that joins allocations, occurrences and appointments to create
+   * a per prisoner per date individual appointment instance. This resulting appointment instance uses the allocation id
+   * as there is a one-to-one relationship between allocations and instances. This is therefore the id that should be
+   * used for the /appointment-instances/:id endpoint. Appointment instances are compatible with NOMIS appointments
+   * stored in the OFFENDER_IND_SCHEDULES table. This event and associated endpoint are used primarily for sync purposes.
+   */
   @PostUpdate
   fun onUpdate(entity: AppointmentOccurrenceAllocation) {
     runCatching {
@@ -50,6 +58,13 @@ class AppointmentOccurrenceAllocationEntityListener {
     }
   }
 
+  /**
+   * Appointment instances are provided by a view that joins allocations, occurrences and appointments to create
+   * a per prisoner per date individual appointment instance. This resulting appointment instance uses the allocation id
+   * as there is a one-to-one relationship between allocations and instances. This is therefore the id that should be
+   * used for the /appointment-instances/:id endpoint. Appointment instances are compatible with NOMIS appointments
+   * stored in the OFFENDER_IND_SCHEDULES table. This event and associated endpoint are used primarily for sync purposes.
+   */
   @PostRemove
   fun onDelete(entity: AppointmentOccurrenceAllocation) {
     runCatching {
