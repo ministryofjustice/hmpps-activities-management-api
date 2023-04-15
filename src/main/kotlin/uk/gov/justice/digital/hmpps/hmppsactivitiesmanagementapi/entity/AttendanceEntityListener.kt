098f020aefc2bf8e521740668775e585e52342b1
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2023-04-15 15:53:28 +0100

    SAA-588: Added attendance created and amended entity listener, events and feature switches.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AttendanceEntityListener.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity
+
+import jakarta.persistence.PostPersist
+import jakarta.persistence.PostUpdate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.OutboundEvent
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.OutboundEventsService
+
+@Component
+class AttendanceEntityListener {
+
+  @Autowired
+  private lateinit var outboundEventsService: OutboundEventsService
+
+  companion object {
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  @PostPersist
+  fun onCreate(attendance: Attendance) {
+    send(
+      OutboundEvent.PRISONER_ATTENDANCE_CREATED,
+      attendance.attendanceId,
+      "Failed to send prisoner attendance created event for attendance ID ${attendance.attendanceId}",
+    )
+  }
+
+  @PostUpdate
+  fun onUpdate(attendance: Attendance) {
+    send(
+      OutboundEvent.PRISONER_ATTENDANCE_AMENDED,
+      attendance.attendanceId,
+      "Failed to send prisoner attendance amended event for attendance ID ${attendance.attendanceId}",
+    )
+  }
+
+  private fun send(outboundEvent: OutboundEvent, attendanceId: Long, failureMessage: String) {
+    runCatching {
+      outboundEventsService.send(outboundEvent, attendanceId)
+    }.onFailure {
+      log.error(failureMessage, it)
+    }
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AttendanceEntityListenerTest.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity
+
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.attendance
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.OutboundEvent
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.OutboundEventsService
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
+class AttendanceEntityListenerTest(@Autowired private val listener: AttendanceEntityListener) {
+
+  @MockBean
+  private lateinit var outboundEventsService: OutboundEventsService
+  private val attendance = attendance()
+
+  @Test
+  fun `prisoner attendance created event raised on creation`() {
+    listener.onCreate(attendance)
+    verify(outboundEventsService).send(OutboundEvent.PRISONER_ATTENDANCE_CREATED, attendance.attendanceId)
+    verifyNoMoreInteractions(outboundEventsService)
+  }
+
+  @Test
+  fun `prisoner attendance amended event raised on update`() {
+    listener.onUpdate(attendance)
+    verify(outboundEventsService).send(OutboundEvent.PRISONER_ATTENDANCE_AMENDED, attendance.attendanceId)
+    verifyNoMoreInteractions(outboundEventsService)
+  }
+}
