72db86fe33a20497d349b00362e9b8a497827282
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-04-21 17:45:36 +0100

    Working appointment occurrence search using specifications

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentOccurrenceSearchRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentOccurrenceSearchRepository.kt
@@ -1,28 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository
 
-import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentOccurrenceSearch
-import java.time.LocalDate
-import java.time.LocalTime
-import org.springframework.stereotype.Repository as RepositoryAnnotation
 
-@RepositoryAnnotation
-interface AppointmentOccurrenceSearchRepository : ReadOnlyRepository<AppointmentOccurrenceSearch, Long> {
-  @Query(
-    value =
-    "FROM AppointmentOccurrenceSearch aos " +
-      "WHERE aos.prisonCode = :prisonCode" +
-      "  AND (:categoryCode IS NULL OR aos.categoryCode = :categoryCode)" +
-      "  AND (:internalLocationId IS NULL OR aos.internalLocationId = :internalLocationId)" +
-      "  AND (:startDate IS NULL OR aos.startDate = :startDate)" +
-      "  AND (:earliestStartTime IS NULL OR :latestStartTime IS NULL OR aos.startTime BETWEEN :earliestStartTime AND :latestStartTime)",
-  )
-  fun find(
-    prisonCode: String,
-    categoryCode: String? = null,
-    internalLocationId: Long? = null,
-    startDate: LocalDate? = null,
-    earliestStartTime: LocalTime? = null,
-    latestStartTime: LocalTime? = null,
-  ): List<AppointmentOccurrenceSearch>
+@Repository
+interface AppointmentOccurrenceSearchRepository :
+  ReadOnlyRepository<AppointmentOccurrenceSearch, Long>,
+  JpaSpecificationExecutor<AppointmentOccurrenceSearch> {
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AppointmentOccurrenceSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AppointmentOccurrenceSearchService.kt
@@ -1,11 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service
 
+import jakarta.persistence.EntityManager
+import jakarta.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentOccurrenceSearch
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.toResults
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentOccurrenceSearchRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.response.AppointmentOccurrenceSearchResult
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentOccurrenceSearchRepository
 import java.security.Principal
+import java.time.LocalDate
 
 @Service
 class AppointmentOccurrenceSearchService(
@@ -19,9 +24,24 @@ class AppointmentOccurrenceSearchService(
     request: AppointmentOccurrenceSearchRequest,
     principal: Principal,
   ): List<AppointmentOccurrenceSearchResult> {
-    val timeRange = request.timeSlot?.let { prisonRegimeService.getTimeRangeForPrisonAndTimeSlot(prisonCode, it) }
+    var spec = Specification<AppointmentOccurrenceSearch>{ root, _, cb -> cb.equal(root.get<String>("prisonCode"), prisonCode) }
 
-    val results = appointmentOccurrenceSearchRepository.find(prisonCode, request.categoryCode, request.internalLocationId, request.startDate, timeRange?.start, timeRange?.end)
+    request.categoryCode?.apply {
+      spec = spec.and { root, _, cb -> cb.equal(root.get<String>("categoryCode"), request.categoryCode) }
+    }
+
+    request.internalLocationId?.apply {
+      spec = spec.and { root, _, cb -> cb.equal(root.get<Long>("internalLocationId"), request.internalLocationId) }
+    }
+
+    spec = spec.and { root, _, cb -> cb.equal(root.get<LocalDate>("startDate"), request.startDate!!) }
+
+    request.timeSlot?.apply {
+      val timeRange = request.timeSlot.let { prisonRegimeService.getTimeRangeForPrisonAndTimeSlot(prisonCode, it) }
+      spec = spec.and { root, _, cb -> cb.between(root.get("startTime"), timeRange.start, timeRange.end) }
+    }
+
+    val results = appointmentOccurrenceSearchRepository.findAll(spec)
 
     val referenceCodeMap = referenceCodeService.getReferenceCodesMap(ReferenceCodeDomain.APPOINTMENT_CATEGORY)
 
