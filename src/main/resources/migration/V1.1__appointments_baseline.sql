38cd156b32bbd30e009d7c06ed9762199ba20235
Simon Oldham <simon.oldham@gdsgroup.com>
2023-04-28 11:24:06 +0100

    Juggling DDL around

--- src/main/resources/migration/V1.1__appointments_baseline.sql
+++ src/main/resources/migration/V1.1__appointments_baseline.sql
@@ -75,6 +75,18 @@ CREATE INDEX idx_appointment_occurrence_allocation_appointment_occurrence_id ON 
 CREATE INDEX idx_appointment_occurrence_allocation_prisoner_number ON appointment_occurrence_allocation (prisoner_number);
 CREATE INDEX idx_appointment_occurrence_allocation_booking_id ON appointment_occurrence_allocation (booking_id);
 
+CREATE TABLE bulk_appointment (
+        bulk_appointment_id        bigserial   NOT NULL CONSTRAINT bulk_appointment_pk PRIMARY KEY,
+        created                    timestamp,
+        created_by                 varchar(100)
+);
+
+CREATE TABLE bulk_appointment_appointment (
+        bulk_appointment_appointment_id        bigserial   NOT NULL CONSTRAINT bulk_appointment_appointment_pk PRIMARY KEY,
+        bulk_appointment_id                    bigint      NOT NULL REFERENCES bulk_appointment (bulk_appointment_id) ON DELETE CASCADE,
+        appointment_id                         bigint      NOT NULL REFERENCES appointment (appointment_id) ON DELETE CASCADE
+);
+
 CREATE OR REPLACE VIEW v_appointment_instance AS
     SELECT
         aoa.appointment_occurrence_allocation_id AS appointment_instance_id,
@@ -102,14 +114,4 @@ CREATE OR REPLACE VIEW v_appointment_instance AS
         LEFT JOIN appointment_cancellation_reason acr on ao.cancellation_reason_id = acr.appointment_cancellation_reason_id;
 
 
-CREATE TABLE bulk_appointment (
-        bulk_appointment_id        bigserial   NOT NULL CONSTRAINT bulk_appointment_pk PRIMARY KEY,
-        created                    timestamp,
-        created_by                 varchar(100)
-);
 
-CREATE TABLE bulk_appointment_appointment (
-        bulk_appointment_appointment_id        bigserial   NOT NULL CONSTRAINT bulk_appointment_appointment_pk PRIMARY KEY,
-        bulk_appointment_id                    bigint      NOT NULL REFERENCES bulk_appointment (bulk_appointment_id) ON DELETE CASCADE,
-        appointment_id                         bigint      NOT NULL REFERENCES appointment (appointment_id) ON DELETE CASCADE
-);
