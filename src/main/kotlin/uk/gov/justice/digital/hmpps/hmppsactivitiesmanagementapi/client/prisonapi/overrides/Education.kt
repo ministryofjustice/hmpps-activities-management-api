944c9e1ed410d3a6e924bd7ee713d6fcc06ef676
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2023-05-08 11:56:11 +0100

    SAA-606: Override the Education class to fix nullable fields.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/client/prisonapi/overrides/Education.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.prisonapi.overrides
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.prisonapi.model.AddressDto
+import java.time.LocalDate
+
+// Overridden because some fields are required to be nullable despite the required=true option.
+
+data class Education(
+  @get:JsonProperty("bookingId", required = true) val bookingId: Long? = null,
+  @get:JsonProperty("startDate", required = true) val startDate: LocalDate? = null,
+  @get:JsonProperty("endDate", required = true) val endDate: LocalDate? = null,
+  @get:JsonProperty("studyArea", required = true) val studyArea: String? = null,
+  @get:JsonProperty("educationLevel", required = true) val educationLevel: String? = null,
+  @get:JsonProperty("numberOfYears", required = true) val numberOfYears: Int? = null,
+  @get:JsonProperty("graduationYear", required = true) val graduationYear: String? = null,
+  @get:JsonProperty("comment", required = true) val comment: String? = null,
+  @get:JsonProperty("school", required = true) val school: String? = null,
+  @get:JsonProperty("isSpecialEducation", required = true) val isSpecialEducation: Boolean? = null,
+  @get:JsonProperty("schedule", required = true) val schedule: String? = null,
+  @get:JsonProperty("addresses", required = true) val addresses: List<AddressDto>? = emptyList(),
+)
