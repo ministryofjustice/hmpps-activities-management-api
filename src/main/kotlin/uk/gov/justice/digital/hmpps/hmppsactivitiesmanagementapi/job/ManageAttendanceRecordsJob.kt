19842ab8c6a2e3c2d4202aacce858a91d5862430
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2023-05-18 17:49:51 +0100

    SAA-668 updating the manage attendnaces job to check for attendance records that need locking down. (#338)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ManageAttendanceRecordsJob.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ManageAttendanceRecordsJob.kt
@@ -1,27 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.job
 
-import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.AttendanceOperation
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.ManageAttendancesService
 
 /**
- * This job creates attendances on the date of execution for instances scheduled and allocations active on this date.
+ * This job is responsible for creating (new) daily attendance records and locking existing (old) attendance records.
  *
  * At present, we do also create attendance records for suspended schedules but not for cancelled schedules. As we learn
  * more this will likely change the behaviour of this job.
  */
 @Component
 class ManageAttendanceRecordsJob(private val attendancesService: ManageAttendancesService) {
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
-  }
-
   @Async("asyncExecutor")
   fun execute() {
     attendancesService.attendances(AttendanceOperation.CREATE)
-    // TODO lock attendances
+    attendancesService.attendances(AttendanceOperation.LOCK)
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AttendanceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AttendanceRepository.kt
@@ -1,9 +1,27 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Attendance
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ScheduledInstance
+import java.time.LocalDate
 
 interface AttendanceRepository : JpaRepository<Attendance, Long> {
   fun existsAttendanceByScheduledInstanceAndPrisonerNumber(scheduledInstance: ScheduledInstance, prisonerNumber: String): Boolean
+
+  /**
+   * Caution should be used with this query.
+   *
+   * Over time the number of attendance records will be significant so the date boundaries used should not be large.
+   */
+  @Query(
+    """
+      SELECT a from Attendance a
+      WHERE a.scheduledInstance.activitySchedule.activity.prisonCode = :prisonCode
+      AND a.status != 'LOCKED'
+      AND a.scheduledInstance.sessionDate >= :startDate
+      AND a.scheduledInstance.sessionDate <= :endDate
+    """,
+  )
+  fun findUnlockedAttendancesAtPrisonBetweenDates(prisonCode: String, startDate: LocalDate, endDate: LocalDate): List<Attendance>
 }
