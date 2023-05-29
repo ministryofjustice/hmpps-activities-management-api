74e0a1c63857aecdf2717d0e126be15ef24116a2
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2023-05-29 17:36:11 +0100

    SAA-689: Ktlint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/EventReviewController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/EventReviewController.kt
@@ -16,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
@@ -27,7 +28,6 @@ import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.response.
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.EventReviewService
 import java.security.Principal
 import java.time.LocalDate
-import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 @RequestMapping("/event-review", produces = [MediaType.APPLICATION_JSON_VALUE])
