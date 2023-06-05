d765968e70c9e3ed8600e33dd65e361873a68459
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-06-05 17:13:06 +0100

    SAA-815 API dependency updates including 3rd party security patches/fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
 import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4-beta-3"
-  kotlin("plugin.spring") version "1.8.10"
-  kotlin("plugin.jpa") version "1.8.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
+  kotlin("plugin.spring") version "1.8.21"
+  kotlin("plugin.jpa") version "1.8.21"
   jacoco
-  id("org.openapi.generator") version "6.5.0"
+  id("org.openapi.generator") version "6.6.0"
 }
 
 allOpen {
@@ -29,8 +29,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0-beta-14")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.25.0")
 
@@ -51,8 +51,8 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("net.javacrumbs.json-unit:json-unit:2.37.0")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.37.0")
-  testImplementation("net.javacrumbs.json-unit:json-unit-json-path:2.37.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.38.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-json-path:2.38.0")
 }
 
 java {
