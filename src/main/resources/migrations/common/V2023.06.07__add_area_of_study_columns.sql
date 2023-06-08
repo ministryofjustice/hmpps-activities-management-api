503cfa9a3d2145bfce75e93a1266eaa84414b5c0
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-06-08 16:00:46 +0100

    SAA-669: Fixed migration

--- src/main/resources/migrations/common/V2023.06.07__add_area_of_study_columns.sql
+++ src/main/resources/migrations/common/V2023.06.07__add_area_of_study_columns.sql
@@ -1,6 +1,8 @@
-ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_code varchar(10) NOT NULL;
-ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_description varchar(60) NOT NULL;
+ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_code varchar(10);
+ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_description varchar(60);
 DROP INDEX idx_activity_minimum_edu_level_activity_edu_code;
 CREATE UNIQUE INDEX idx_activity_minimum_edu_level_activity_edu_code ON activity_minimum_education_level (activity_id, study_area_code, education_level_code);
 UPDATE activity_minimum_education_level SET study_area_code = 'ENGLA';
 UPDATE activity_minimum_education_level SET study_area_description = 'English Language';
+ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_code varchar(10) NOT NULL;
+ALTER TABLE activity_minimum_education_level ADD COLUMN study_area_description varchar(60) NOT NULL;
