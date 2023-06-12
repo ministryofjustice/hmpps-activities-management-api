688b69407ffef6c19474d248b9dad773c4e3850d
gerryallan-moj <123087637+gerryallan-moj@users.noreply.github.com>
2023-06-12 17:01:14 +0100

    SAA-664: Add dates when creating allocation (#378)
    * SAA-716: new patch endpoint to update allocations
    
    * SAA-716: new patch endpoint to update allocations
    
    * SAA-716: new patch endpoint to update allocations
    
    * SAA-664: and dates to new allocations
    
    * SAA-664: and dates to new allocations
    
    * SAA-664: and dates to new allocations
    
    * SAA-664: and dates to new allocations
    
    * SAA-664: and dates to new allocations

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/PrisonerAllocationRequestTest.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request
+
+import jakarta.validation.ConstraintViolation
+import jakarta.validation.Validation
+import jakarta.validation.Validator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.TimeSource
+
+class PrisonerAllocationRequestTest {
+  private val validator: Validator = Validation.buildDefaultValidatorFactory().validator
+
+  @Test
+  fun `start date must be in the future`() {
+    val request = PrisonerAllocationRequest(
+      prisonerNumber = "G4793VF",
+      payBandId = 11,
+      startDate = TimeSource.today(),
+    )
+    assertSingleValidationError(validator.validate(request), "startDate", "Start date must be in the future")
+  }
+
+  private fun assertSingleValidationError(validate: MutableSet<ConstraintViolation<PrisonerAllocationRequest>>, propertyName: String, message: String) {
+    assertThat(validate.size).isEqualTo(1)
+    assertThat(validate.first().propertyPath.toString()).isEqualTo(propertyName)
+    assertThat(validate.first().message).isEqualTo(message)
+  }
+}
