9d09ab744218d6906e3992166220c74399cc5b12
Simon Oldham <simon.oldham@gdsgroup.com>
2023-06-14 21:44:13 +0100

    Removed accidentally comitted spike debris

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivityRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivityRepository.kt
@@ -10,24 +10,13 @@ import java.time.LocalDate
 
 @Repository
 interface ActivityRepository : JpaRepository<Activity, Long> {
-  @Query(
-    value = "from Activity a " +
-      "left join ActivitySchedule s on s.startDate >= :earliestScheduleStartDate " +
-      "where a.prisonCode = :prison " +
-      "and a.startDate <= :date " +
-      "and (a.endDate is null or a.endDate >= :date)",
-  )
-  fun getAllForPrisonAndDate(
-    @Param("prison") prison: String,
-    @Param("date") date: LocalDate,
-    @Param("earliestScheduleStartDate") earliestScheduleStartDate: LocalDate = LocalDate.now().minusDays(28),
-  ): List<Activity>
+  @Query(value = "from Activity a where a.prisonCode = :prison and a.startDate <= :date and (a.endDate is null or a.endDate >= :date)")
+  fun getAllForPrisonAndDate(@Param("prison") prison: String, @Param("date") date: LocalDate): List<Activity>
 
   @Query(
     value =
     """
     from Activity a
-    left join ActivitySchedule s on s.startDate >= :earliestScheduleStartDate 
     where a.prisonCode = :prisonCode
     and a.startDate <= :toDate
     and (a.endDate is null or a.endDate >= :fromDate)
@@ -37,21 +26,8 @@ interface ActivityRepository : JpaRepository<Activity, Long> {
     @Param("prisonCode") prisonCode: String,
     @Param("fromDate") fromDate: LocalDate,
     @Param("toDate") toDate: LocalDate,
-    @Param("earliestScheduleStartDate") earliestScheduleStartDate: LocalDate = LocalDate.now().minusDays(28),
   ): List<Activity>
 
-  @Query(
-    value =
-    """
-    from Activity a
-    left join ActivitySchedule s on s.startDate >= :earliestScheduledStartDate 
-    where activityId = :id
-    """,
-  )
-  fun getLimited(
-    @Param("id")id: Long,
-    @Param("earliestScheduledStartDate") earliestScheduledStartDate: LocalDate = LocalDate.now().minusDays(28),
-  ): Activity
   fun getAllByPrisonCodeAndActivityCategory(prisonCode: String, category: ActivityCategory): List<Activity>
   fun getAllByPrisonCode(prisonCode: String): List<Activity>
   fun existsActivityByPrisonCodeAndSummary(prisonCode: String, summary: String): Boolean
