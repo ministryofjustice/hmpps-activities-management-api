30c9b5169c22b9bd78eb821e6835c86fce1fe938
Lee Jacobson <125488090+leejacobson-moj@users.noreply.github.com>
2023-06-23 15:49:45 +0100

    SAA-872: Removes duplicate transformer (#415)
    Co-authored-by: Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/Attendance.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/Attendance.kt
@@ -16,11 +16,8 @@ import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.Fetch
 import org.hibernate.annotations.FetchMode
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.util.transform
 import java.time.LocalDate
 import java.time.LocalDateTime
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.Attendance as ModelAttendance
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.AttendanceReason as ModelAttendanceReason
 
 @Entity
 @Table(name = "attendance")
@@ -183,44 +180,6 @@ data class Attendance(
         this.scheduledInstance.sessionDate.isAfter(LocalDate.now().minusDays(14))
       )
   }
-
-  fun toModel(caseNoteText: String?) = ModelAttendance(
-    id = this.attendanceId,
-    scheduleInstanceId = this.scheduledInstance.scheduledInstanceId,
-    prisonerNumber = this.prisonerNumber,
-    attendanceReason = this.attendanceReason?.let {
-      ModelAttendanceReason(
-        id = it.attendanceReasonId,
-        code = it.code.toString(),
-        description = it.description,
-        attended = it.attended,
-        capturePay = it.capturePay,
-        captureMoreDetail = it.captureMoreDetail,
-        captureCaseNote = it.captureCaseNote,
-        captureIncentiveLevelWarning = it.captureIncentiveLevelWarning,
-        captureOtherText = it.captureOtherText,
-        displayInAbsence = it.displayInAbsence,
-        displaySequence = it.displaySequence,
-        notes = it.notes,
-      )
-    },
-    comment = this.comment,
-    recordedTime = this.recordedTime,
-    recordedBy = this.recordedBy,
-    status = this.status.name,
-    payAmount = this.payAmount,
-    bonusAmount = this.bonusAmount,
-    pieces = this.pieces,
-    issuePayment = this.issuePayment,
-    incentiveLevelWarningIssued = this.incentiveLevelWarningIssued,
-    otherAbsenceReason = this.otherAbsenceReason,
-    caseNoteText = caseNoteText,
-    attendanceHistory = this.attendanceHistory
-      .sortedWith(compareBy { it.recordedTime })
-      .reversed()
-      .map { attendanceHistoryRow -> transform(attendanceHistoryRow, caseNoteText) },
-    editable = this.editable(),
-  )
 }
 
 enum class AttendanceStatus {
