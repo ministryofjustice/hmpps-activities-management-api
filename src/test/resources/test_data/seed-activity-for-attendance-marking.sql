77f157e21bb2bf616fb6860f26b36f561c835b9b
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-06-23 09:23:04 +0100

    SAA-890: Adds validation to attendance marking

--- src/test/resources/test_data/seed-activity-for-attendance-marking.sql
+++ src/test/resources/test_data/seed-activity-for-attendance-marking.sql
@@ -1,10 +1,17 @@
+INSERT INTO prison_regime (prison_regime_id, prison_code, am_start, am_finish, pm_start, pm_finish, ed_start, ed_finish)
+VALUES(3, 'PVI', '10:00:00', '11:00:00', '13:00:00', '16:30:00', '18:00:00', '20:00:00');
+
 insert into activity(activity_id, prison_code, activity_category_id, activity_tier_id, attendance_required, in_cell, piece_work, outside_work, pay_per_session, summary, description, start_date, end_date, risk_level, minimum_incentive_nomis_code, minimum_incentive_level, created_time, created_by)
 values (1, 'PVI', 1, 1, true, false, false, false, 'H', 'Maths', 'Maths Level 1', '2022-10-10', null, 'high', 'BAS', 'Basic', '2022-9-21 00:00:00', 'SEED USER');
 
 insert into activity_pay(activity_pay_id, activity_id, incentive_nomis_code, incentive_level, prison_pay_band_id, rate, piece_rate, piece_rate_items)
-values (1, 1, 'BAS', 'Basic', 1, 125, 125, 1),
-       (2, 1, 'STD', 'Standard', 2, 150, 150, 1),
-       (3, 1, 'ENH', 'Enhanced', 3, 175, 175, 1);
+values (1, 1, 'BAS', 'Basic', 1, 125, 150, 1);
+
+insert into activity_pay(activity_pay_id, activity_id, incentive_nomis_code, incentive_level, prison_pay_band_id, rate, piece_rate, piece_rate_items)
+values (2, 1, 'BAS', 'Basic', 2, 225, 250, 1);
+
+insert into activity_pay(activity_pay_id, activity_id, incentive_nomis_code, incentive_level, prison_pay_band_id, rate, piece_rate, piece_rate_items)
+values (3, 1, 'BAS', 'Basic', 3, 325, 350, 1);
 
 insert into activity_minimum_education_level(activity_minimum_education_level_id, activity_id, education_level_code, education_level_description, study_area_code, study_area_description)
 values (1, 1, '1', 'Reading Measure 1.0', 'ENGLA', 'English Language');
@@ -43,7 +50,10 @@ insert into scheduled_instance(activity_schedule_id, session_date, start_time, e
 values (1, CURRENT_DATE, '10:00:00', '11:00:00', false, null, null, null, null);
 
 insert into attendance(attendance_id, scheduled_instance_id, prisoner_number, attendance_reason_id, comment, recorded_time, recorded_by, status, pay_amount, bonus_amount, pieces)
-values (1, 1, 'A11111A', 9, null, CURRENT_TIMESTAMP, 'MR BLOGS', 'COMPLETED', null, null, null);
+values (1, 1, 'A11111A', null, null, null, null, 'WAITING', null, null, null);
+
+insert into attendance(attendance_id, scheduled_instance_id, prisoner_number, attendance_reason_id, comment, recorded_time, recorded_by, status, pay_amount, bonus_amount, pieces)
+values (2, 1, 'A22222A', null, null, null, null, 'WAITING', null, null, null);
 
 insert into scheduled_instance(activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment)
 values (2, CURRENT_DATE, '14:00:00', '15:00:00', false, null, null, null, null);
