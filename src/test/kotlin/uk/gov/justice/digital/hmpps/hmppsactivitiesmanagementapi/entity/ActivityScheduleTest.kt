93a24eda67bfffe57dcda840c0c4b21ba93ad9a6
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2023-06-27 09:28:51 +0100

    SAA-875 apply activity end date changes to the associated schedules, allocations and planned deallocations. (#421)
    * SAA-875 apply activity end date changes to the associated schedules, allocations and planned deallocations.
    
    * SAA-875 adding test to check ended allocations are ignored when end date of schedule is changed.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/ActivityScheduleTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/ActivityScheduleTest.kt
@@ -925,4 +925,68 @@ class ActivityScheduleTest {
       assertThat(hasNoInstancesOnDate(tomorrow, slotOne.startTime to slotOne.endTime)).isTrue
     }
   }
+
+  @Test
+  fun `change to schedule end date is applied to allocations`() {
+    val schedule =
+      activitySchedule(activity = activityEntity(), noAllocations = true, startDate = yesterday, endDate = tomorrow)
+
+    schedule.allocatePrisoner(
+      prisonerNumber = "1111111".toPrisonerNumber(),
+      payBand = lowPayBand,
+      bookingId = 10001,
+      allocatedBy = "FRED",
+      endDate = tomorrow,
+    )
+
+    schedule.allocatePrisoner(
+      prisonerNumber = "2222222".toPrisonerNumber(),
+      payBand = lowPayBand,
+      bookingId = 20002,
+      allocatedBy = "BILL",
+      endDate = tomorrow,
+    )
+
+    assertThat(schedule.allocations()).hasSize(2)
+
+    schedule.allocations().forEach { allocation -> assertThat(allocation.endDate).isEqualTo(tomorrow) }
+
+    schedule.endDate = today
+
+    schedule.allocations().forEach { allocation -> assertThat(allocation.endDate).isEqualTo(today) }
+  }
+
+  @Test
+  fun `change to schedule end date is is not applied to ended allocations`() {
+    val schedule =
+      activitySchedule(
+        activity = activityEntity(),
+        noAllocations = true,
+        startDate = yesterday,
+        endDate = tomorrow.plusDays(1),
+      )
+
+    val activeAllocation = schedule.allocatePrisoner(
+      prisonerNumber = "1111111".toPrisonerNumber(),
+      payBand = lowPayBand,
+      bookingId = 10001,
+      allocatedBy = "FRED",
+      endDate = tomorrow.plusDays(1),
+    )
+
+    val endedAllocation = schedule.allocatePrisoner(
+      prisonerNumber = "2222222".toPrisonerNumber(),
+      payBand = lowPayBand,
+      bookingId = 20002,
+      allocatedBy = "BILL",
+      endDate = tomorrow.plusDays(1),
+    ).deallocateNow()
+
+    assertThat(schedule.allocations()).hasSize(2)
+
+    schedule.endDate = tomorrow
+
+    assertThat(activeAllocation.endDate).isEqualTo(tomorrow)
+    assertThat(endedAllocation.endDate).isEqualTo(today)
+  }
 }
