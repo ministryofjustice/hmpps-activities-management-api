6e2e0fd94b033f1b5d5f5cc84dd04c3e3a696be9
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-07-20 14:27:39 +0100

    SAA-940: Pay band sequence value had not been set correctly

--- src/main/resources/migrations/dev/V2023.07.20__rollout_risley.sql
+++ src/main/resources/migrations/dev/V2023.07.20__rollout_risley.sql
@@ -6,6 +6,8 @@ values ('RSI', 'HMP Risley', true, '2023-07-20', true, '2023-07-20');
 insert into prison_regime (prison_code, am_start, am_finish, pm_start, pm_finish, ed_start, ed_finish)
 VALUES ('RSI', '08:30:00', '11:45:00', '13:45:00', '16:45:00', '17:30:00', '19:15:00');
 
+SELECT setval('prison_pay_band_prison_pay_band_id_seq', (SELECT MAX(prison_pay_band_id) FROM prison_pay_band), true);
+
 insert into prison_pay_band (display_sequence, nomis_pay_band, pay_band_alias, pay_band_description, prison_code)
 values (1, 1, 'Low', 'Pay band 1 (Lowest)', 'RSI'),
        (2, 2, 'Medium', 'Pay band 2', 'RSI'),
