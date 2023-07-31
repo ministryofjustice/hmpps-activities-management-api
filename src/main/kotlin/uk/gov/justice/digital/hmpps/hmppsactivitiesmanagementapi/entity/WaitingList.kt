288d01769051739c5d052abf78f3a665cb439b83
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-07-31 16:34:08 +0100

    SAA-1057 introduction of new entity support prisoner waitlist functionality.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/WaitingList.kt
@@ -0,0 +1,59 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "waiting_list")
+class WaitingList(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val waitingListId: Long = 0,
+
+  val prisonCode: String,
+
+  val prisonerNumber: String,
+
+  val bookingId: Long,
+
+  val applicationDate: LocalDate,
+
+  @OneToOne
+  @JoinColumn(name = "activity_schedule_id", nullable = false)
+  val activitySchedule: ActivitySchedule,
+
+  val requestedBy: String,
+
+  @Enumerated(EnumType.STRING)
+  var status: WaitingListStatus,
+
+  val createdBy: String,
+) {
+  val creationTime: LocalDateTime = LocalDateTime.now()
+
+  var declinedReason: String? = null
+
+  var updatedTime: LocalDateTime? = null
+
+  var updatedBy: String? = null
+
+  @OneToOne
+  @JoinColumn(name = "allocation_id", nullable = false)
+  var allocation: Allocation? = null
+}
+
+enum class WaitingListStatus {
+  PENDING,
+  APPROVED,
+  DECLINED,
+  // TODO add more statuses
+}
---
+++ src/main/resources/migrations/common/V2023.07.31__add_waiting_list.sql
@@ -0,0 +1,20 @@
+CREATE TABLE waiting_list (
+  waiting_list_id      bigserial    NOT NULL CONSTRAINT waiting_list_pk PRIMARY KEY,
+  prison_code          varchar(3)   NOT NULL,
+  prisoner_number      varchar(7)   NOT NULL,
+  booking_id           bigint       NOT NULL,
+  application_date     date         NOT NULL,
+  activity_id          bigint       NOT NULL,
+  activity_schedule_id bigint       NOT NULL,
+  requested_by         varchar(100) NOT NULL,
+  status               varchar(20)  NOT NULL,
+  creation_time        timestamp    NOT NULL,
+  created_by           varchar(20)  NOT NULL,
+  comments             varchar(300),
+  declined_reason      varchar(100),
+  updated_time         timestamp,
+  updated_by           varchar(100),
+  allocation_id        bigint REFERENCES allocation (allocation_id)
+);
+
+CREATE INDEX idx_waiting_list_allocation_id ON waiting_list (allocation_id);
