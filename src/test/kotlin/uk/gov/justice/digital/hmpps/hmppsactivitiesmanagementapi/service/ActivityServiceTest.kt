e2370cb1514fb965946bdc54c367656e9e70fb30
gerryallan-moj <123087637+gerryallan-moj@users.noreply.github.com>
2023-08-07 18:52:24 +0100

    SAA-1065 - Duplicate activity name validation in "Edit activity" jour… (#501)
    * SAA-1065 - Duplicate activity name validation in "Edit activity" journey compares and activity to itself
    
    * SAA-1065 - Duplicate activity name validation in "Edit activity" journey compares and activity to itself

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ActivityServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ActivityServiceTest.kt
@@ -594,9 +594,7 @@ class ActivityServiceTest {
     ).thenReturn(savedActivityEntity)
     whenever(activityRepository.existsActivityByPrisonCodeAndSummary(any(), any())).thenReturn(true)
 
-    val updateDuplicateActivityRequest: ActivityUpdateRequest = mock {
-      on { summary } doReturn ("IT level 1")
-    }
+    val updateDuplicateActivityRequest: ActivityUpdateRequest = mapper.read("activity/activity-update-request-5.json")
 
     assertThatThrownBy {
       service().updateActivity(
@@ -607,7 +605,7 @@ class ActivityServiceTest {
       )
     }
       .isInstanceOf(IllegalArgumentException::class.java)
-      .hasMessage("Duplicate activity name detected for this prison (MDI): 'IT level 1'")
+      .hasMessage("Duplicate activity name detected for this prison (MDI): 'IT level 2'")
 
     verify(activityRepository, never()).saveAndFlush(any())
   }
