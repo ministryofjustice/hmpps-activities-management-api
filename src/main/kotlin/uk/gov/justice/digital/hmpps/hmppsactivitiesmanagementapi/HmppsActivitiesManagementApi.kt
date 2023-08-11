5586a6158869fd7dff3acc8f42bf4377ef464bab
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-08-11 16:51:16 +0100

    SAA-864 Log key events

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/HmppsActivitiesManagementApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/HmppsActivitiesManagementApi.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
