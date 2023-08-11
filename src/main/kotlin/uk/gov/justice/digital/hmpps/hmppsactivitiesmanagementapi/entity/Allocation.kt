c98531f8ba3cfce61d9db92b4dffc5735031c149
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-08-11 17:08:21 +0100

    SAA-864 Log key events

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/Allocation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/Allocation.kt
@@ -127,6 +127,7 @@ data class Allocation(
 
   private fun maybeEndDate() =
     when {
+      endDate != null -> endDate
       activitySchedule.endDate != null -> activitySchedule.endDate
       activitySchedule.activity.endDate != null -> activitySchedule.activity.endDate
       else -> null
