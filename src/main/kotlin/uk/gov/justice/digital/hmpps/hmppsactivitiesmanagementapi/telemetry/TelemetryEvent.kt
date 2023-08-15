f56c6a9503df517b0004b1323d35595c7bfb3220
Simon Oldham <simon.oldham@gdsgroup.com>
2023-08-15 22:04:18 +0100

    Repordering enum vals

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/telemetry/TelemetryEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/telemetry/TelemetryEvent.kt
@@ -3,11 +3,11 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry
 enum class TelemetryEvent(val value: String) {
 
   ACTIVITY_CREATED("SAA-CreateActivity"),
-  EDIT_ACTIVITY("SAA-EditActivity"),
   COC("SAA-ChangeOfCircumstances"),
+  EDIT_ACTIVITY("SAA-EditActivity"),
   PRISONER_ADDED_TO_WAITLIST("SAA-PrisonerAddedToWaitlist"),
   PRISONER_ALLOCATED("SAA-PrisonerAllocated"),
+  PRISONER_APPROVED_ON_WAITLIST("SAA-PrisonerApporvedOnWaitlist"),
   PRISONER_DEALLOCATED("SAA-PrisonerDeallocated"),
   PRISONER_DECLINED_FROM_WAITLIST("SAA-PrisonerDeclinedFromWaitlist"),
-  PRISONER_APPROVED_ON_WAITLIST("SAA-PrisonerApporvedOnWaitlist"),
 }
