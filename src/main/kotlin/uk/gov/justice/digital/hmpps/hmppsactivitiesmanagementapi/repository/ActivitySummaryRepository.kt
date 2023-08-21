9dc512e630fe26c28708d6b7a9eb59ef63ca7333
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-08-21 18:32:59 +0100

    review

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivitySummaryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivitySummaryRepository.kt
@@ -1,9 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository
 
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ActivitySummary
-import org.springframework.stereotype.Repository as RepositoryAnnotation
+import org.springframework.stereotype.Repository
 
-@RepositoryAnnotation
+@Repository
 interface ActivitySummaryRepository : ReadOnlyRepository<ActivitySummary, Long> {
   fun findAllByPrisonCode(prisonCode: String): List<ActivitySummary>
 }
