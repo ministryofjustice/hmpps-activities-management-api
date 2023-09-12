f5929969f78f4989699952ff936ecfa9826d5019
Simon Oldham <simon.oldham@gdsgroup.com>
2023-09-12 16:19:43 +0100

    Removed unnecessary @Transactional annotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ActivityMetricsJob.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ActivityMetricsJob.kt
@@ -5,7 +5,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ActivityCategory
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ActivityTier
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.JobType
@@ -58,7 +57,6 @@ class ActivityMetricsJob(
   }
 
   @Async("asyncExecutor")
-  @Transactional(readOnly = true)
   fun execute() {
     jobRunner.runJob(
       JobDefinition(JobType.ACTIVITIES_METRICS) {
