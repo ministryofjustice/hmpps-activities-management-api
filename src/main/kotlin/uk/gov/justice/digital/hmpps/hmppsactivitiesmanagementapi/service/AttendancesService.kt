41bf37f2d8fffb102d03f1198bb7d566d4d5c259
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-09-15 16:31:17 +0100

    SAA-1199 some preliminary refactoring to the mark attendance prior to introducing validation.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AttendancesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/AttendancesService.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.casenotesapi.api.CaseNotesApiClient
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.casenotesapi.model.CaseNote
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AttendanceStatus
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AttendanceReasonEnum
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.toModel
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AttendanceUpdateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AllAttendanceRepository
@@ -16,7 +16,6 @@ import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.find
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.util.transform
 import java.time.LocalDate
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.AllAttendance as ModelAllAttendance
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.Attendance as ModelAttendance
 
 @Service
 @Transactional(readOnly = true)
@@ -37,38 +36,44 @@ class AttendancesService(
 
   @Transactional
   fun mark(principalName: String, attendances: List<AttendanceUpdateRequest>) {
-    val attendanceUpdatesById = attendances.associateBy { it.id }
-    val attendanceReasonsByCode = attendanceReasonRepository.findAll().associateBy { it.code.toString() }
+    val attendanceUpdatesById = attendances.onEach(AttendanceUpdateRequestValidator::validate).associateBy { it.id }
+    val attendanceReasonsByCode = attendanceReasonRepository.findAll().associateBy { it.code }
 
-    val updatedAttendances = attendanceRepository.findAllById(attendanceUpdatesById.keys).mapNotNull {
-      var caseNoteDetails: CaseNote? = null
-      if (!attendanceUpdatesById.containsKey(it.attendanceId)) { throw IllegalArgumentException("Attendance record not found") }
-      if (!attendanceUpdatesById[it.attendanceId]!!.caseNote.isNullOrEmpty()) {
-        caseNoteDetails = caseNotesApiClient.postCaseNote(attendanceUpdatesById[it.attendanceId]!!.prisonCode, it.prisonerNumber, attendanceUpdatesById[it.attendanceId]!!.caseNote!!, attendanceUpdatesById[it.attendanceId]!!.incentiveLevelWarningIssued)
-      }
-      it.mark(
-        principalName,
-        attendanceReasonsByCode[attendanceUpdatesById[it.attendanceId]!!.attendanceReason?.uppercase()?.trim()],
-        attendanceUpdatesById[it.attendanceId]?.status ?: AttendanceStatus.COMPLETED,
-        attendanceUpdatesById[it.attendanceId]!!.comment,
-        attendanceUpdatesById[it.attendanceId]!!.issuePayment,
-        attendanceUpdatesById[it.attendanceId]!!.incentiveLevelWarningIssued,
-        caseNoteDetails?.caseNoteId,
-        attendanceUpdatesById[it.attendanceId]!!.otherAbsenceReason,
+    val updatedAttendances = attendanceRepository.findAllById(attendanceUpdatesById.keys).mapNotNull { attendance ->
+      val updateRequest = attendanceUpdatesById[attendance.attendanceId]!!
+
+      attendance.mark(
+        principalName = principalName,
+        reason = attendanceReasonsByCode[updateRequest.maybeAttendanceReason()],
+        newStatus = updateRequest.status,
+        newComment = updateRequest.comment,
+        newIssuePayment = updateRequest.issuePayment,
+        newIncentiveLevelWarningIssued = updateRequest.incentiveLevelWarningIssued,
+        newCaseNoteId = updateRequest.mayBeCaseNote(attendance.prisonerNumber)?.caseNoteId,
+        newOtherAbsenceReason = updateRequest.otherAbsenceReason,
       )
     }
 
     attendanceRepository.saveAll(updatedAttendances)
   }
 
-  fun getAttendanceById(id: Long): ModelAttendance {
-    val attendance = attendanceRepository.findOrThrowNotFound(id)
-    return transform(
-      attendance,
-      caseNotesApiClient,
-    )
-  }
+  private fun AttendanceUpdateRequest.mayBeCaseNote(prisonerNumber: String): CaseNote? =
+    caseNote?.let {
+      caseNotesApiClient.postCaseNote(prisonCode, prisonerNumber, caseNote, incentiveLevelWarningIssued)
+    }
+
+  private fun AttendanceUpdateRequest.maybeAttendanceReason() =
+    attendanceReason?.let { AttendanceReasonEnum.valueOf(it.trim().uppercase()) }
+
+  fun getAttendanceById(id: Long) =
+    transform(attendanceRepository.findOrThrowNotFound(id), caseNotesApiClient)
 
   fun getAllAttendanceByDate(prisonCode: String, sessionDate: LocalDate): List<ModelAllAttendance> =
     allAttendanceRepository.findByPrisonCodeAndSessionDate(prisonCode, sessionDate).toModel()
+}
+
+internal object AttendanceUpdateRequestValidator {
+  fun validate(request: AttendanceUpdateRequest) {
+    // TODO introduce validation of attendance update
+  }
 }
