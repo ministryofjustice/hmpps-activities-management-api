c0c8a8159a5ce02e6c26dae89ca9d86ac9fe9293
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-09-25 14:00:06 +0100

    SAA-1214: Integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/MigrateAppointmentIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/MigrateAppointmentIntegrationTest.kt
@@ -3,11 +3,16 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.integration
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.within
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.get
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentType
@@ -16,7 +21,9 @@ import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.isEqual
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.AppointmentInstance
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentMigrateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.PrisonerSearchPrisonerFixture
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.events.AppointmentInstanceInformation
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.events.OutboundEventsPublisher
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.events.OutboundHMPPSDomainEvent
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -25,6 +32,7 @@ import java.time.temporal.ChronoUnit
 @TestPropertySource(
   properties = [
     "feature.event.appointments.appointment-instance.created=true",
+    "feature.event.appointments.appointment-instance.deleted=true",
   ],
 )
 @Transactional(readOnly = true)
@@ -32,6 +40,8 @@ class MigrateAppointmentIntegrationTest : IntegrationTestBase() {
 
   @MockBean
   private lateinit var eventsPublisher: OutboundEventsPublisher
+
+  private val eventCaptor = argumentCaptor<OutboundHMPPSDomainEvent>()
 
   @Test
   fun `migrate appointment forbidden`() {
@@ -104,6 +114,59 @@ class MigrateAppointmentIntegrationTest : IntegrationTestBase() {
     }
   }
 
+  @Test
+  fun `delete migrated appointments - forbidden`() {
+    val error = webTestClient.delete()
+      .uri("/migrate-appointment/MDI?startDate=2023-09-25")
+      .headers(setAuthorisation(roles = listOf()))
+      .exchange()
+      .expectStatus().isForbidden
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody(ErrorResponse::class.java)
+      .returnResult().responseBody
+
+    with(error!!) {
+      assertThat(status).isEqualTo(403)
+      assertThat(userMessage).isEqualTo("Access denied: Access Denied")
+      assertThat(developerMessage).isEqualTo("Access Denied")
+    }
+  }
+
+  @Sql(
+    "classpath:test_data/seed-migrated-appointments.sql",
+  )
+  @Test
+  fun `delete migrated appointments - success`() {
+    webTestClient.deleteMigratedAppointments("MDI", LocalDate.of(2023, 9, 25))
+
+    verify(eventsPublisher, times(2)).send(eventCaptor.capture())
+
+    assertThat(
+      eventCaptor.allValues.map { it.eventType }.distinct().single(),
+    ).isEqualTo("appointments.appointment-instance.deleted")
+    assertThat(eventCaptor.allValues.map { it.additionalInformation }).contains(
+      AppointmentInstanceInformation(25),
+      AppointmentInstanceInformation(27),
+    )
+  }
+
+  @Sql(
+    "classpath:test_data/seed-migrated-appointments.sql",
+  )
+  @Test
+  fun `delete migrated chaplaincy appointments - success`() {
+    webTestClient.deleteMigratedAppointments("MDI", LocalDate.of(2023, 9, 25), "CHAP")
+
+    verify(eventsPublisher).send(eventCaptor.capture())
+
+    assertThat(
+      eventCaptor.allValues.map { it.eventType }.distinct().single(),
+    ).isEqualTo("appointments.appointment-instance.deleted")
+    assertThat(eventCaptor.allValues.map { it.additionalInformation }).contains(
+      AppointmentInstanceInformation(25),
+    )
+  }
+
   private fun WebTestClient.migrateAppointment(
     request: AppointmentMigrateRequest,
   ) =
@@ -116,4 +179,18 @@ class MigrateAppointmentIntegrationTest : IntegrationTestBase() {
       .expectHeader().contentType(MediaType.APPLICATION_JSON)
       .expectBody(AppointmentInstance::class.java)
       .returnResult().responseBody
+
+  private fun WebTestClient.deleteMigratedAppointments(
+    prisonCode: String,
+    startDate: LocalDate,
+    categoryCode: String? = null,
+  ) {
+    delete()
+      .uri("/migrate-appointment/$prisonCode?startDate=$startDate" + (categoryCode?.let { "&categoryCode=$categoryCode" } ?: ""))
+      .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_APPOINTMENTS")))
+      .exchange()
+      .expectStatus().isAccepted
+
+    Thread.sleep(1000)
+  }
 }
