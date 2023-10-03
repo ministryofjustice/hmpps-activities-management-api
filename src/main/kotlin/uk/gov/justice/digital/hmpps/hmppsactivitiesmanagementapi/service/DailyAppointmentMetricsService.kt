861a0b2d48cd6de917afe3b8d2e67ee9ec56ec1c
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-10-03 16:47:46 +0100

    SAA-1230: Move logic and transaction into service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/DailyAppointmentMetricsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/DailyAppointmentMetricsService.kt
@@ -1,19 +1,51 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service
 
+import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Appointment
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_INSTANCE_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_SERIES_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_SET_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.CANCELLED_APPOINTMENT_COUNT_METRIC_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.CATEGORY_CODE_PROPERTY_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.DELETED_APPOINTMENT_COUNT_METRIC_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.PRISON_CODE_PROPERTY_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.TelemetryEvent
+import java.time.LocalDate
 
 @Service
-class DailyAppointmentMetricsService {
+@Transactional(readOnly = true)
+class DailyAppointmentMetricsService(
+  private val appointmentRepository: AppointmentRepository,
+  private val telemetryClient: TelemetryClient,
+) {
+  fun generateAppointmentMetrics(prisonCode: String, categoryCode: String, date: LocalDate) {
+    val propertiesMap = mapOf(
+      PRISON_CODE_PROPERTY_KEY to prisonCode,
+      CATEGORY_CODE_PROPERTY_KEY to categoryCode,
+    )
 
-  fun generateAppointmentMetrics(metricsMap: MutableMap<String, Double>, appointments: List<Appointment>) {
-    appointments.forEach {
+    val metricsMap = mutableMapOf(
+      APPOINTMENT_COUNT_METRIC_KEY to 0.0,
+      APPOINTMENT_INSTANCE_COUNT_METRIC_KEY to 0.0,
+      APPOINTMENT_SERIES_COUNT_METRIC_KEY to 0.0,
+      APPOINTMENT_SET_COUNT_METRIC_KEY to 0.0,
+      CANCELLED_APPOINTMENT_COUNT_METRIC_KEY to 0.0,
+      DELETED_APPOINTMENT_COUNT_METRIC_KEY to 0.0,
+    )
+
+    appointmentRepository.findByPrisonCodeAndCategoryCodeAndDate(prisonCode, categoryCode, date).apply {
+      this.generateAppointmentMetrics(metricsMap)
+    }
+
+    telemetryClient.trackEvent(TelemetryEvent.APPOINTMENTS_AGGREGATE_METRICS.value, propertiesMap, metricsMap)
+  }
+
+  private fun List<Appointment>.generateAppointmentMetrics(metricsMap: MutableMap<String, Double>) {
+    this.forEach {
       if (!it.isCancelled() && !it.isDeleted) {
         incrementMetric(metricsMap, APPOINTMENT_COUNT_METRIC_KEY)
 
