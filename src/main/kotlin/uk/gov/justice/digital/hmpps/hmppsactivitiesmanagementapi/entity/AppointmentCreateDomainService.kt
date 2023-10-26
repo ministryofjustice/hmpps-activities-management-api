b91f5f54af26b948ee3e372f86ecbb8c6fcf9f32
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-10-26 15:07:59 +0100

    SAA-1256: Refactor migrate appointment

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/entity/AppointmentCreateDomainService.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class AppointmentCreateDomainService {
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/MigrateAppointmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/MigrateAppointmentService.kt
@@ -5,12 +5,22 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.common.ifNotEmpty
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentCreateDomainService
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentCancelDomainService
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentType
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentSeries
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.AppointmentInstance
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.ApplyTo.THIS_AND_ALL_FUTURE_APPOINTMENTS
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentCancelRequest
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AppointmentMigrateRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentSeriesRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentSeriesSpecification
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentTierRepository
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentCancellationReasonRepository
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.NOT_SPECIFIED_APPOINTMENT_TIER_ID
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.DELETE_MIGRATED_APPOINTMENT_CANCELLATION_REASON_ID
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.findOrThrowNotFound
+import java.security.Principal
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -19,10 +29,70 @@ import java.time.LocalDateTime
 class MigrateAppointmentService(
   private val appointmentSeriesSpecification: AppointmentSeriesSpecification,
   private val appointmentSeriesRepository: AppointmentSeriesRepository,
+  private val appointmentTierRepository: AppointmentTierRepository,
+  private val appointmentCancellationReasonRepository: AppointmentCancellationReasonRepository,
+  private val appointmentCreateDomainService: AppointmentCreateDomainService,
   private val appointmentCancelDomainService: AppointmentCancelDomainService,
 ) {
   companion object {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  fun migrateAppointment(request: AppointmentMigrateRequest): AppointmentInstance {
+    val appointmentTier = appointmentTierRepository.findOrThrowNotFound(NOT_SPECIFIED_APPOINTMENT_TIER_ID)
+
+    val appointmentSeries = appointmentSeriesRepository.saveAndFlush(
+      AppointmentSeries(
+        appointmentType = AppointmentType.INDIVIDUAL,
+        prisonCode = request.prisonCode!!,
+        categoryCode = request.categoryCode!!,
+        customName = request.comment?.trim()?.take(40),
+        appointmentTier = appointmentTier,
+        internalLocationId = request.internalLocationId,
+        startDate = request.startDate!!,
+        startTime = request.startTime!!,
+        endTime = request.endTime,
+        extraInformation = request.comment?.trim()?.takeIf { it.length > 40 },
+        createdTime = request.created!!,
+        createdBy = request.createdBy!!,
+        updatedTime = request.updated,
+        updatedBy = request.updatedBy,
+        isMigrated = true,
+      )
+    )
+
+    val =  appointmentCreateDomainService.createAppointments(
+      appointmentSeries,
+      mapOf(request.prisonerNumber!! to request.bookingId!!),
+      isCancelled = request.isCancelled ?: false
+    )
+
+    val prisonerBookings = mapOf(request.prisonerNumber!! to request.bookingId.toString())
+    val appointmentSeries = appointmentSeriesRepository.saveAndFlush(
+      buildValidAppointmentSeriesEntity(
+        appointmentType = AppointmentType.INDIVIDUAL,
+        prisonCode = request.prisonCode!!,
+        prisonerNumbers = listOf(request.prisonerNumber),
+        prisonerBookings = prisonerBookings,
+        categoryCode = request.categoryCode!!,
+        customName = request.comment?.trim()?.take(40),
+        appointmentTier = appointmentTier,
+        internalLocationId = request.internalLocationId,
+        inCell = false,
+        startDate = request.startDate,
+        startTime = request.startTime,
+        endTime = request.endTime,
+        extraInformation = request.comment?.trim()?.takeIf { it.length > 40 },
+        createdTime = request.created!!,
+        createdBy = request.createdBy!!,
+        updatedTime = request.updated,
+        updatedBy = request.updatedBy,
+        isCancelled = request.isCancelled ?: false,
+        isMigrated = true,
+      ),
+    )
+    val appointmentInstance = appointmentInstanceRepository.findOrThrowNotFound(appointmentSeries.appointments().first().attendees().first().appointmentAttendeeId)
+    return appointmentInstance.toModel()
   }
 
   fun deleteMigratedAppointments(prisonCode: String, startDate: LocalDate, categoryCode: String? = null) {
