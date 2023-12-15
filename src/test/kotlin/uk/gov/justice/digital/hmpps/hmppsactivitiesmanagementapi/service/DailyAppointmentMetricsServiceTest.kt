1c2b1dc4cedb95e7f7fbdb9c66934eff0a2ccc12
Lee Jacobson <125488090+leejacobson-moj@users.noreply.github.com>
2023-12-15 09:58:42 +0000

    SAA-1432: Added appointment attendance to daily metrics (#725)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/DailyAppointmentMetricsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/DailyAppointmentMetricsServiceTest.kt
@@ -8,16 +8,21 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Appointment
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentAttendee
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentSeries
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentSeriesSchedule
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentSet
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.isEqualTo
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.helpers.moorlandPrisonCode
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentAttendeeRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository.AppointmentRepository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_INSTANCE_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_SERIES_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.APPOINTMENT_SET_COUNT_METRIC_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.ATTENDANCE_RECORDED_ATTENDED_COUNT_METRIC_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.ATTENDANCE_RECORDED_COUNT_METRIC_KEY
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.ATTENDANCE_RECORDED_NOT_ATTENDED_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.CANCELLED_APPOINTMENT_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.DELETED_APPOINTMENT_COUNT_METRIC_KEY
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.telemetry.TelemetryEvent
@@ -25,12 +30,17 @@ import java.time.LocalDate
 
 class DailyAppointmentMetricsServiceTest {
   private val appointmentRepository: AppointmentRepository = mock()
+  private val appointmentAttendeeRepository: AppointmentAttendeeRepository = mock()
   private val telemetryClient: TelemetryClient = mock()
 
   private val telemetryPropertyMap = argumentCaptor<Map<String, String>>()
   private val telemetryMetricsMap = argumentCaptor<Map<String, Double>>()
 
-  private val dailyAppointmentMetricsService = DailyAppointmentMetricsService(appointmentRepository, telemetryClient)
+  private val dailyAppointmentMetricsService = DailyAppointmentMetricsService(
+    appointmentRepository,
+    appointmentAttendeeRepository,
+    telemetryClient,
+  )
 
   private val cancelledAppointment: Appointment = mock()
   private val deletedAppointment: Appointment = mock()
@@ -41,6 +51,8 @@ class DailyAppointmentMetricsServiceTest {
   private val appointmentSeriesWithSet: AppointmentSeries = mock()
   private val appointmentSeriesSchedule: AppointmentSeriesSchedule = mock()
   private val emptyAppointmentSeries: AppointmentSeries = mock()
+  private val attendedAttendee: AppointmentAttendee = mock()
+  private val notAttendedAttendee: AppointmentAttendee = mock()
 
   @Test
   fun `should generate daily metrics`() {
@@ -63,6 +75,9 @@ class DailyAppointmentMetricsServiceTest {
     whenever(appointmentWithAttendees.appointmentSeries).thenReturn(emptyAppointmentSeries)
     whenever(appointmentWithAttendees.attendees()).thenReturn(listOf(mock(), mock(), mock()))
 
+    whenever(attendedAttendee.attended).thenReturn(true)
+    whenever(notAttendedAttendee.attended).thenReturn(false)
+
     val appointments = listOf(
       cancelledAppointment,
       deletedAppointment,
@@ -70,9 +85,17 @@ class DailyAppointmentMetricsServiceTest {
       appointmentWithSeries,
       appointmentWithAttendees,
     )
+
+    val appointmentAttendance = listOf(
+      attendedAttendee,
+      notAttendedAttendee,
+    )
+
     val yesterday = LocalDate.now().minusDays(1)
 
     whenever(appointmentRepository.findByPrisonCodeAndCategoryCodeAndDate(moorlandPrisonCode, "CHAP", yesterday)).thenReturn(appointments)
+
+    whenever(appointmentAttendeeRepository.findByPrisonCodeAndCategoryAndRecordedDate(moorlandPrisonCode, "CHAP", yesterday)).thenReturn(appointmentAttendance)
 
     dailyAppointmentMetricsService.generateAppointmentMetrics(moorlandPrisonCode, "CHAP", yesterday)
 
@@ -85,6 +108,9 @@ class DailyAppointmentMetricsServiceTest {
       this[APPOINTMENT_SET_COUNT_METRIC_KEY] isEqualTo 1.0
       this[CANCELLED_APPOINTMENT_COUNT_METRIC_KEY] isEqualTo 1.0
       this[DELETED_APPOINTMENT_COUNT_METRIC_KEY] isEqualTo 1.0
+      this[ATTENDANCE_RECORDED_COUNT_METRIC_KEY] isEqualTo 2.0
+      this[ATTENDANCE_RECORDED_ATTENDED_COUNT_METRIC_KEY] isEqualTo 1.0
+      this[ATTENDANCE_RECORDED_NOT_ATTENDED_COUNT_METRIC_KEY] isEqualTo 1.0
     }
   }
 }
