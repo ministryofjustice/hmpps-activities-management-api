3d2a10f8d039b7fb2974bffb5b1f6ed7262ae022
Lance Munslow <lance.munslow@digital.justice.gov.uk>
2024-02-14 14:26:38 +0000

    SAA-1510: Remove redundant check as it happens during prior transform

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ManageAttendanceRecordsJob.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/job/ManageAttendanceRecordsJob.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.job
 
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.common.onOrBefore
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.JobType
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.RolloutPrisonPlan
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.ManageAttendancesService
@@ -49,5 +48,5 @@ class ManageAttendanceRecordsJob(
     (
       mayBePrisonCode?.let { listOf(rolloutPrisonService.getByPrisonCode(it)) }
         ?: rolloutPrisonService.getRolloutPrisons()
-      ).filter { it.activitiesRolledOut && it.activitiesRolloutDate.onOrBefore(LocalDate.now()) }.map(RolloutPrisonPlan::prisonCode)
+      ).filter { it.activitiesRolledOut }.map(RolloutPrisonPlan::prisonCode)
 }
