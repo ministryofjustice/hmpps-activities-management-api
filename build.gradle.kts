f0d39bcf8d0e66f3f04bb0f56329e5f149001ba5
Lance Munslow <lance.munslow@digital.justice.gov.uk>
2024-03-22 13:55:32 +0000

    SAA-1638: Update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.4"
   kotlin("plugin.spring") version "1.9.22"
   kotlin("plugin.jpa") version "1.9.22"
   jacoco
