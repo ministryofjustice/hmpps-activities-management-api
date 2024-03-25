f1dcd418047bf1611d993b6a3930d7e951ef2ee2
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2024-03-25 12:13:46 +0000

    SAA-1639 Increase the bank holiday cache to 7 days. Leaving the prison incentive levels cache at 1 day

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/CacheConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/CacheConfiguration.kt
@@ -22,16 +22,23 @@ class CacheConfiguration {
     return ConcurrentMapCacheManager(BANK_HOLIDAYS_CACHE_NAME, PRISON_INCENTIVE_LEVELS_CACHE_NAME)
   }
 
-  @CacheEvict(allEntries = true, cacheNames = [BANK_HOLIDAYS_CACHE_NAME, PRISON_INCENTIVE_LEVELS_CACHE_NAME])
-  @Scheduled(fixedDelay = TTL_HOURS, timeUnit = TimeUnit.HOURS)
-  fun cacheEvict() {
-    log.info("Evicting caches: $BANK_HOLIDAYS_CACHE_NAME, $PRISON_INCENTIVE_LEVELS_CACHE_NAME")
+  @CacheEvict(value = [PRISON_INCENTIVE_LEVELS_CACHE_NAME])
+  @Scheduled(fixedDelay = TTL_HOURS_PRISONER_INCENTIVE_LEVELS, timeUnit = TimeUnit.HOURS)
+  fun cacheEvictPrisonerIncentiveLevels() {
+    log.info("Evicting cache: $PRISON_INCENTIVE_LEVELS_CACHE_NAME")
+  }
+
+  @CacheEvict(value = [BANK_HOLIDAYS_CACHE_NAME])
+  @Scheduled(fixedDelay = TTL_HOURS_BANK_HOLIDAYS, timeUnit = TimeUnit.HOURS)
+  fun cacheEvictBankHolidays() {
+    log.info("Evicting cache: $BANK_HOLIDAYS_CACHE_NAME")
   }
 
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
     const val BANK_HOLIDAYS_CACHE_NAME: String = "bankHolidays"
     const val PRISON_INCENTIVE_LEVELS_CACHE_NAME: String = "prisonIncentiveLevels"
-    const val TTL_HOURS: Long = 24
+    const val TTL_HOURS_PRISONER_INCENTIVE_LEVELS: Long = 24
+    const val TTL_HOURS_BANK_HOLIDAYS: Long = 24 * 7
   }
 }
