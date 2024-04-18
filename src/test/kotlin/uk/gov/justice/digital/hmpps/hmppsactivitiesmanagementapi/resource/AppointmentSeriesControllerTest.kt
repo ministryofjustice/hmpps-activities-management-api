bd0607d2fdb8c9c55db0cca9255da34f46c1fd6f
Lance Munslow <lance.munslow@digital.justice.gov.uk>
2024-04-18 16:36:09 +0100

    SAA-1661: Add attendance required flag to attendance summary

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/AppointmentSeriesControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/AppointmentSeriesControllerTest.kt
@@ -52,6 +52,9 @@ class AppointmentSeriesControllerTest : ControllerTestBase<AppointmentSeriesCont
     val response = mockMvc.getAppointmentSeriesById(-1)
       .andExpect { content { contentType(MediaType.APPLICATION_JSON_VALUE) } }
       .andExpect { status { isNotFound() } }
+      .andReturn().response
+
+    assertThat(response.contentAsString).contains("Appointment Series -1 not found")
 
     verify(appointmentSeriesService).getAppointmentSeriesById(-1)
   }
