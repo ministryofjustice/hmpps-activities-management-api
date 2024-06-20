fb9353edd7b3f28b944c7f686b86b26d3e5b206a
tim <wright.tim.j@gmail.com>
2024-06-20 16:32:51 +0100

    SAA-1792 new endpoint to return appointment attendance by prisoner and status for a given day

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentRepository.kt
@@ -2,10 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Appointment
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AppointmentSeries
 import java.time.LocalDate
+import java.time.LocalTime
 
 @Repository
 interface AppointmentRepository : JpaRepository<Appointment, Long> {
@@ -21,4 +23,53 @@ interface AppointmentRepository : JpaRepository<Appointment, Long> {
     nativeQuery = true,
   )
   fun findByPrisonCodeAndCategoryCodeAndDate(prisonCode: String, categoryCode: String, startDate: LocalDate): List<Appointment>
+
+  @Query(
+    value = """
+      SELECT 
+      aa.prisoner_number,
+      aa.booking_id,
+      a.appointment_id,
+      aa.appointment_attendee_id,
+      a.start_date,
+      a.start_time,
+      a.end_time,
+      et.code AS event_tier,
+      a.category_code,
+      a.custom_name,
+      aa.attended
+      FROM appointment a
+      JOIN appointment_attendee aa ON aa.appointment_id = a.appointment_id
+      LEFT JOIN event_tier et on et.event_tier_id = a.appointment_tier_id
+      WHERE a.start_date = :date
+        AND a.prison_code = :prisonCode
+        AND (:categoryCode IS NULL OR a.category_code = :categoryCode)
+        AND (:customName IS NULL OR a.custom_name = :customName)
+        AND (:prisonerNumber IS NULL OR aa.prisoner_number = :prisonerNumber)
+        AND ((NOT :isCancelled AND a.cancellation_reason_id IS NULL) OR (:isCancelled AND a.cancellation_reason_id IS NOT NULL))
+    """,
+    nativeQuery = true,
+  )
+  fun getAppointmentsWithAttendees(
+    @Param("prisonCode") prisonCode: String,
+    @Param("date") date: LocalDate,
+    @Param("categoryCode") categoryCode: String?,
+    @Param("customName") customName: String?,
+    @Param("prisonerNumber") prisonerNumber: String?,
+    @Param("isCancelled") isCancelled: Boolean,
+  ): List<AppointmentAndAttendee>
+}
+
+interface AppointmentAndAttendee {
+  fun getPrisonerNumber(): String
+  fun getBookingId(): Long
+  fun getAppointmentId(): Long
+  fun getAppointmentAttendeeId(): Long
+  fun getStartDate(): LocalDate
+  fun getStartTime(): LocalTime
+  fun getEndTime(): LocalTime
+  fun getEventTier(): String
+  fun getCategoryCode(): String
+  fun getCustomName(): String?
+  fun getAttended(): Boolean?
 }
---
+++ src/test/resources/test_data/seed-appointment-attendance-by-status.sql
@@ -0,0 +1,16 @@
+INSERT INTO appointment_series_schedule (appointment_series_schedule_id, frequency, number_of_appointments)
+VALUES (1, 'DAILY', 3);
+
+INSERT INTO appointment_series (appointment_series_id, appointment_type, prison_code, category_code, appointment_tier_id, internal_location_id, in_cell, start_date, start_time, end_time, appointment_series_schedule_id, created_time, created_by)
+VALUES (1, 'GROUP', 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', 1, (now()::date - 2)::timestamp, 'TEST.USER');
+
+
+INSERT INTO appointment (appointment_id, appointment_series_id, sequence_number, prison_code, category_code, appointment_tier_id, internal_location_id, in_cell, start_date, start_time, end_time, created_time, created_by, cancelled_time, cancellation_reason_id, cancelled_by, is_deleted, custom_name)
+VALUES  (1, 1, 1, 'RSI', 'EDUC', 2, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', (now()::date - 1)::timestamp, 2, 'CANCEL.USER', false, null),
+        (2, 1, 3, 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', null, null, null, false, 'custom');
+
+INSERT INTO appointment_attendee (appointment_attendee_id, appointment_id, prisoner_number, booking_id, attended)
+VALUES  (1, 2, 'A1234BC', 1, null),
+        (2, 2, 'B2345CD', 2, true),
+        (4, 1, 'B2346CD', 4, null),
+        (3, 2, 'C3456DE', 3, false);
