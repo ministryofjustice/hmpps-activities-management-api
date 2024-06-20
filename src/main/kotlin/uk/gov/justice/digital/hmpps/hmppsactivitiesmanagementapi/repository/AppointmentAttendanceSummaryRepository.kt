85efff9b1ac5b315df1ae99ec10c9a0c4594268d
Tim Wright <wright.tim.j@gmail.com>
2024-06-20 11:04:59 +0100

    SAA-1797 move filters to jpa and use category code (#917)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentAttendanceSummaryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/AppointmentAttendanceSummaryRepository.kt
@@ -7,4 +7,7 @@ import java.time.LocalDate
 @Repository
 interface AppointmentAttendanceSummaryRepository : ReadOnlyRepository<AppointmentAttendanceSummary, Long> {
   fun findByPrisonCodeAndStartDate(prisonCode: String, startDate: LocalDate): List<AppointmentAttendanceSummary>
+  fun findByPrisonCodeAndStartDateAndCategoryCode(prisonCode: String, startDate: LocalDate, categoryCode: String): List<AppointmentAttendanceSummary>
+  fun findByPrisonCodeAndStartDateAndCustomName(prisonCode: String, startDate: LocalDate, customName: String): List<AppointmentAttendanceSummary>
+  fun findByPrisonCodeAndStartDateAndCategoryCodeAndCustomName(prisonCode: String, startDate: LocalDate, categoryCode: String, customName: String): List<AppointmentAttendanceSummary>
 }
--- src/test/resources/test_data/seed-appointment-attendance-summaries.sql
+++ src/test/resources/test_data/seed-appointment-attendance-summaries.sql
@@ -11,10 +11,10 @@ VALUES (1, 'DAILY', 3);
 INSERT INTO appointment_series (appointment_series_id, appointment_type, prison_code, category_code, appointment_tier_id, internal_location_id, in_cell, start_date, start_time, end_time, appointment_series_schedule_id, created_time, created_by)
 VALUES (1, 'GROUP', 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', 1, (now()::date - 2)::timestamp, 'TEST.USER');
 
-INSERT INTO appointment (appointment_id, appointment_series_id, sequence_number, prison_code, category_code, appointment_tier_id, internal_location_id, in_cell, start_date, start_time, end_time, created_time, created_by, cancelled_time, cancellation_reason_id, cancelled_by, is_deleted)
-VALUES  (1, 1, 1, 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', (now()::date - 1)::timestamp, 2, 'CANCEL.USER', false),
-        (2, 1, 2, 'RSI', 'EDUC', 1, 123, false, now()::date, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', null, null, null, false),
-        (3, 1, 3, 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', null, null, null, false);
+INSERT INTO appointment (appointment_id, appointment_series_id, sequence_number, prison_code, category_code, appointment_tier_id, internal_location_id, in_cell, start_date, start_time, end_time, created_time, created_by, cancelled_time, cancellation_reason_id, cancelled_by, is_deleted, custom_name)
+VALUES  (1, 1, 1, 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', (now()::date - 1)::timestamp, 2, 'CANCEL.USER', false, null),
+        (2, 1, 2, 'RSI', 'EDUC', 1, 123, false, now()::date, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', null, null, null, false, 'custom'),
+        (3, 1, 3, 'RSI', 'EDUC', 1, 123, false, now()::date - 1, '09:00', '10:30', (now()::date - 2)::timestamp, 'TEST.USER', null, null, null, false, null);
 
 INSERT INTO appointment_attendee (appointment_attendee_id, appointment_id, prisoner_number, booking_id)
 VALUES  (1, 1, 'A1234BC', 1),
