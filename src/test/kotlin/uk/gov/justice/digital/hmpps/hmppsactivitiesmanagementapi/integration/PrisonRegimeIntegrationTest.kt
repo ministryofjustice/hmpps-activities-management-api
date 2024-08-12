fd3936a641ea70e5ac720e6f6de44d7c42c42195
Tim Wright <wright.tim.j@gmail.com>
2024-08-12 13:12:58 +0100

    regime to return times for each day (#1002)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/PrisonRegimeIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/PrisonRegimeIntegrationTest.kt
@@ -16,14 +16,14 @@ class PrisonRegimeIntegrationTest : IntegrationTestBase() {
   fun `RSI regime reflects single regime`() {
     val regime = getPrisonRegime(agencyId = "RSI")
 
-    assertThat(regime.size).isEqualTo(1)
-    assertThat(regime.first().amStart).isEqualTo(LocalTime.of(9, 0, 0))
-    assertThat(regime.first().amFinish).isEqualTo(LocalTime.of(12, 0, 0))
-    assertThat(regime.first().pmStart).isEqualTo(LocalTime.of(13, 45, 0))
-    assertThat(regime.first().pmFinish).isEqualTo(LocalTime.of(16, 45, 0))
-    assertThat(regime.first().edStart).isEqualTo(LocalTime.of(18, 0, 0))
-    assertThat(regime.first().edFinish).isEqualTo(LocalTime.of(20, 0, 0))
-    assertThat(regime.first().daysOfWeek.containsAll(DayOfWeek.entries)).isTrue()
+    assertThat(regime.size).isEqualTo(7)
+    assertThat(regime.all { it.amStart == LocalTime.of(9, 0, 0) }).isTrue()
+    assertThat(regime.all { it.amFinish == LocalTime.of(12, 0, 0) }).isTrue()
+    assertThat(regime.all { it.pmStart == LocalTime.of(13, 45, 0) }).isTrue()
+    assertThat(regime.all { it.pmFinish == LocalTime.of(16, 45, 0) }).isTrue()
+    assertThat(regime.all { it.edStart == LocalTime.of(18, 0, 0) }).isTrue()
+    assertThat(regime.all { it.edFinish == LocalTime.of(20, 0, 0) }).isTrue()
+    assertThat(regime.map { it.dayOfWeek }).containsAll(DayOfWeek.entries)
   }
 
   @Sql(
@@ -32,23 +32,48 @@ class PrisonRegimeIntegrationTest : IntegrationTestBase() {
   @Test
   fun `get prison regime for IWI with a monday to thursday regime, friday regime and weekend regime`() {
     val regime = getPrisonRegime(agencyId = "IWI")
-    val mondayToThursdayRegime = regime.firstOrNull {
-      it.daysOfWeek.containsAll(listOf(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY))
+
+    assertThat(regime.first().dayOfWeek).isEqualTo(DayOfWeek.MONDAY)
+    assertThat(regime.last().dayOfWeek).isEqualTo(DayOfWeek.SUNDAY)
+
+    val mondayToThursdayRegime = regime.filter {
+      listOf(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY).contains(it.dayOfWeek)
     }
 
     val fridayRegime = regime.firstOrNull {
-      it.daysOfWeek.containsAll(listOf(DayOfWeek.FRIDAY))
+      it.dayOfWeek == DayOfWeek.FRIDAY
     }
 
-    val weekendRegime = regime.firstOrNull {
-      it.daysOfWeek.containsAll(listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY))
+    val weekendRegime = regime.filter {
+      listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(it.dayOfWeek)
     }
 
-    assertThat(regime.size).isEqualTo(3)
+    assertThat(regime.size).isEqualTo(7)
 
-    assertThat(mondayToThursdayRegime).isNotNull
+    assertThat(mondayToThursdayRegime).isNotEmpty
     assertThat(fridayRegime).isNotNull
-    assertThat(weekendRegime).isNotNull
+    assertThat(weekendRegime).isNotEmpty
+
+    assertThat(mondayToThursdayRegime.all { it.amStart == LocalTime.of(9, 25, 0) })
+    assertThat(mondayToThursdayRegime.all { it.amFinish == LocalTime.of(11, 35, 0) })
+    assertThat(mondayToThursdayRegime.all { it.pmStart == LocalTime.of(13, 40, 0) })
+    assertThat(mondayToThursdayRegime.all { it.pmFinish == LocalTime.of(16, 50, 0) })
+    assertThat(mondayToThursdayRegime.all { it.edStart == LocalTime.of(18, 0, 0) })
+    assertThat(mondayToThursdayRegime.all { it.edFinish == LocalTime.of(19, 0, 0) })
+
+    assertThat(weekendRegime.all { it.amStart == LocalTime.of(9, 0, 0) })
+    assertThat(weekendRegime.all { it.amFinish == LocalTime.of(11, 30, 0) })
+    assertThat(weekendRegime.all { it.pmStart == LocalTime.of(13, 40, 0) })
+    assertThat(weekendRegime.all { it.pmFinish == LocalTime.of(16, 45, 0) })
+    assertThat(weekendRegime.all { it.edStart == LocalTime.of(18, 0, 0) })
+    assertThat(weekendRegime.all { it.edFinish == LocalTime.of(19, 0, 0) })
+
+    assertThat(fridayRegime!!.amStart).isEqualTo(LocalTime.of(8, 25, 0))
+    assertThat(fridayRegime.amFinish).isEqualTo(LocalTime.of(11, 35, 0))
+    assertThat(fridayRegime.pmStart).isEqualTo(LocalTime.of(13, 40, 0))
+    assertThat(fridayRegime.pmFinish).isEqualTo(LocalTime.of(16, 50, 0))
+    assertThat(fridayRegime.edStart).isEqualTo(LocalTime.of(18, 0, 0))
+    assertThat(fridayRegime.edFinish).isEqualTo(LocalTime.of(19, 0, 0))
   }
 
   private fun getPrisonRegime(agencyId: String): List<PrisonRegime> = webTestClient.get()
