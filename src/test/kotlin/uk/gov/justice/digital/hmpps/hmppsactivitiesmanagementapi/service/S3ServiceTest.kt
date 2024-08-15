85cd1ba402cd0dc7e71ceab400f596c012504f96
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-08-15 17:31:40 +0100

    ktlintformat changes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/S3ServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/S3ServiceTest.kt
@@ -6,10 +6,8 @@ import aws.sdk.kotlin.services.s3.model.S3Exception
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.springframework.test.context.TestPropertySource
 
 class S3ServiceTest {
   private val amazonS3: S3Client = mockk(relaxed = true)
@@ -37,5 +35,4 @@ class S3ServiceTest {
       runBlocking { service.pushReportToAnalyticalPlatformS3("test-file".toByteArray(), "test-file.csv", "testTable", mockBucketName) }
     }
   }
-
 }
