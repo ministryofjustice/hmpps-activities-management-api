18654aac12e6789b8571653d9ea3f2850182e420
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-08-16 13:13:09 +0100

    Configure Frankland

--- src/main/resources/migrations/common/V2024.08.16__frankland_configuration.sql
+++ src/main/resources/migrations/common/V2024.08.16__frankland_configuration.sql
@@ -1,6 +1,6 @@
 -- Setup HMP Frankland, final dates tbc
 insert into rollout_prison (code, description, activities_to_be_rolled_out, activities_rollout_date, appointments_to_be_rolled_out, appointments_rollout_date)
-values ('FKI', 'HMP Frankland', false, '2024-09-24', false, '2024-08-16');
+values ('FKI', 'HMP Frankland', false, '2024-09-24', false, '2024-09-24');
 
 -- Regime times for HMP Frankland
 insert into prison_regime (prison_code, am_start, am_finish, pm_start, pm_finish, ed_start, ed_finish)
@@ -11,4 +11,10 @@ insert into prison_pay_band (display_sequence, nomis_pay_band, pay_band_alias, p
 values (1,  1,  'Pay band 1 (Lowest)', 'Pay band 1 (Lowest)', 'FKI'),
        (2,  2,  'Pay band 2', 'Pay band 2', 'FKI'),
        (3,  3,  'Pay band 3', 'Pay band 3', 'FKI'),
-       (10, 10, 'Pay band 4 (Highest)', 'Pay band 4 (Highest)', 'FKI');
+       (4,  4,  'Pay band 4', 'Pay band 4', 'FKI'),
+       (5,  5,  'Pay band 5', 'Pay band 5', 'FKI'),
+       (6,  6,  'Pay band 6', 'Pay band 6', 'FKI'),
+       (7,  7,  'Pay band 7', 'Pay band 7', 'FKI'),
+       (8,  8,  'Pay band 8', 'Pay band 8', 'FKI'),
+       (9,  9,  'Pay band 9', 'Pay band 9', 'FKI'),
+       (10, 10, 'Pay band 10 (Highest)', 'Pay band 10 (Highest)', 'FKI');
