fa0e3c3d8bb65add650ea8bd19d646d54d9a3523
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2024-09-05 10:43:04 +0100

    edited int test for allocation count

--- src/test/resources/test_data/seed-activity-id-16.sql
+++ src/test/resources/test_data/seed-activity-id-16.sql
@@ -37,6 +37,9 @@ values (4, 2, 'A11111A', 10001, 3, '2022-10-10', null, '2022-10-10 10:00:00', 'M
 insert into allocation(allocation_id, activity_schedule_id, prisoner_number, booking_id, prison_pay_band_id, start_date, end_date, allocated_time, allocated_by, deallocated_time, deallocated_by, deallocated_reason, suspended_time, suspended_by, suspended_reason, prisoner_status)
 values (5, 2, 'A22222A', 10002, 3, '2022-10-10', null, '2022-10-10 10:00:00', 'MRS BLOGS', null, null, null, null, null, null, 'ACTIVE');
 
+insert into allocation(allocation_id, activity_schedule_id, prisoner_number, booking_id, prison_pay_band_id, start_date, end_date, allocated_time, allocated_by, deallocated_time, deallocated_by, deallocated_reason, suspended_time, suspended_by, suspended_reason, prisoner_status)
+values (6, 1, 'A444444A', 1004, 2, '2022-10-10', null, '2022-10-10 09:00:00', 'MISS BLOGS', null, null, null, null, null, null, 'SUSPENDED');
+
 insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
 values (1, 1, now()::date, '10:00:00', '11:00:00', false, null, null, null, null, 'AM');
 
