989b17a140ebda71d056416373b8ee57aa6c2a0b
Tim Wright <wright.tim.j@gmail.com>
2024-09-11 09:24:46 +0100

    SAA-2010 endpoint to create regime times (#1034)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/RolloutController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/resource/RolloutController.kt
@@ -6,21 +6,40 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.PrisonRegime
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.RolloutPrisonPlan
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.refdata.PrisonRegimeService
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.refdata.RolloutPrisonService
+import java.time.DayOfWeek
+import java.time.LocalTime
+
+data class PrisonRegimeSlot(
+  val dayOfWeek: DayOfWeek,
+  val amStart: LocalTime,
+  val amFinish: LocalTime,
+  val pmStart: LocalTime,
+  val pmFinish: LocalTime,
+  val edStart: LocalTime,
+  val edFinish: LocalTime,
+)
 
 @RestController
 @RequestMapping("/rollout", produces = [MediaType.APPLICATION_JSON_VALUE])
 class RolloutController(
   private val rolloutService: RolloutPrisonService,
+  private val prisonRegimeService: PrisonRegimeService,
 ) {
 
   @GetMapping(value = ["/{prisonCode}"])
@@ -110,4 +129,16 @@ class RolloutController(
   )
   fun getRolledOutPrisons(): List<RolloutPrisonPlan> =
     rolloutService.getRolloutPrisons()
+
+  @Operation(
+    summary = "Creates a prison regime for a given prison",
+    description = "If a regine exists it will overwrite it.  Called via migration dashboard only",
+  )
+  @PostMapping("/prison-regime/{agencyId}")
+  @PreAuthorize("hasRole('ACTIVITY_ADMIN')")
+  @ResponseStatus(HttpStatus.CREATED)
+  fun setPrisonRegimeSlots(
+    @PathVariable("agencyId") agencyId: String,
+    @RequestBody slots: List<PrisonRegimeSlot>,
+  ): List<PrisonRegime> = prisonRegimeService.setPrisonRegime(agencyId = agencyId, slots = slots)
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/RolloutIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/RolloutIntegrationTest.kt
@@ -4,8 +4,12 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.PrisonRegime
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.RolloutPrisonPlan
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.resource.PrisonRegimeSlot
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.resource.ROLE_ACTIVITY_ADMIN
+import java.time.DayOfWeek
+import java.time.LocalTime
 
 class RolloutIntegrationTest : IntegrationTestBase() {
 
@@ -16,6 +20,50 @@ class RolloutIntegrationTest : IntegrationTestBase() {
       assertThat(appointmentsRolledOut).isTrue
     }
   }
+
+  @Test
+  fun `create a regime, then overwrite it`() {
+    val created = webTestClient.createRegime(
+      slots = DayOfWeek.entries.map {
+        createRegimeSlot(dayOfWeek = it, amStart = LocalTime.of(8, 30))
+      },
+    )
+
+    assertThat(created.size).isEqualTo(7)
+    assertThat(created.first { it.dayOfWeek == DayOfWeek.MONDAY }.amStart).isEqualTo(LocalTime.of(8, 30))
+
+    val updated = webTestClient.createRegime(
+      slots = DayOfWeek.entries.map {
+        createRegimeSlot(dayOfWeek = it, amStart = LocalTime.of(9, 30))
+      },
+    )
+
+    assertThat(updated.size).isEqualTo(7)
+    assertThat(updated.first { it.dayOfWeek == DayOfWeek.MONDAY }.amStart).isEqualTo(LocalTime.of(9, 30))
+  }
+
+  private fun createRegimeSlot(dayOfWeek: DayOfWeek, amStart: LocalTime): PrisonRegimeSlot =
+    PrisonRegimeSlot(
+      dayOfWeek = dayOfWeek,
+      amStart = amStart,
+      amFinish = LocalTime.now(),
+      pmStart = LocalTime.now(),
+      pmFinish = LocalTime.now(),
+      edStart = LocalTime.now(),
+      edFinish = LocalTime.now(),
+    )
+
+  private fun WebTestClient.createRegime(slots: List<PrisonRegimeSlot>): List<PrisonRegime> =
+    this.post()
+      .uri("/rollout/prison-regime/TST")
+      .bodyValue(slots)
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf(ROLE_ACTIVITY_ADMIN)))
+      .exchange()
+      .expectStatus().isCreated
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBodyList(PrisonRegime::class.java)
+      .returnResult().responseBody!!
 
   private fun WebTestClient.getPrisonByCode(code: String) =
     get()
