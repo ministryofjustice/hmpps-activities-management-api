394bd86142781eac7de22e484d2db2d7e9362154
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2024-09-27 16:13:38 +0100

    SAA-2045: create a reusable cron job to update paid to unpaid activities including deallocation and re-allocation. Add details of an activity for testing in dev and preprod

--- src/main/resources/migrations/common/V2024.09.27__data_fix_table.sql
+++ src/main/resources/migrations/common/V2024.09.27__data_fix_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE data_fix
 (
-    data_fix_id bigserial,
+    data_fix_id bigserial NOT NULL CONSTRAINT data_fix_pk PRIMARY KEY,
     activity_schedule_id bigint,
     prisoner_number varchar(7),
     start_date date,
