f4ef096054c3a3d16a09f3e509751d42efeb9e4b
Lance Munslow <lance@munslow.net>
2024-10-18 16:22:53 +0100

    SAA-1988: Add non-association boolean when retrieving allocated prisoners (#1100)
    Co-authored-by: Lance Munslow <lance.munslow@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/client/nonassociationsapi/api/NonAssociationsApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/client/nonassociationsapi/api/NonAssociationsApiClient.kt
@@ -2,6 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.nonasso
 
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.util.UriBuilder
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.nonassociationsapi.model.NonAssociation
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.nonassociationsapi.model.PrisonerNonAssociation
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.client.nonassociationsapi.model.PrisonerNonAssociations
 
@@ -13,5 +16,18 @@ class NonAssociationsApiClient(private val nonAssociationsApiWebClient: WebClien
       .retrieve()
       .bodyToMono(PrisonerNonAssociations::class.java)
       .block()?.nonAssociations ?: emptyList()
+  }
+
+  suspend fun getNonAssociationsInvolving(prisonCode: String, prisonerNumbers: List<String>): List<NonAssociation> {
+    return nonAssociationsApiWebClient.post()
+      .uri { uriBuilder: UriBuilder ->
+        uriBuilder
+          .path("/non-associations/involving")
+          .queryParam("prisonId", prisonCode)
+          .build()
+      }
+      .bodyValue(prisonerNumbers)
+      .retrieve()
+      .awaitBody()
   }
 }
