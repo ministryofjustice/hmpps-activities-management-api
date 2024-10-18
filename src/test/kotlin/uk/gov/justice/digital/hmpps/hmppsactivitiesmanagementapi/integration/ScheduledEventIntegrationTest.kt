779b99728bdf1b32bd3a445fb2ccf2f93bca8ca1
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2024-10-18 10:14:54 +0100

    fix integration test for scheduled events

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ScheduledEventIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ScheduledEventIntegrationTest.kt
@@ -472,7 +472,7 @@ class ScheduledEventIntegrationTest : IntegrationTestBase() {
     @Sql("classpath:test_data/seed-activity-with-planned-deallocation-date.sql")
     fun `POST - multiple prisoners - scheduled events returned on planned deallocation date`() {
       val prisonCode = "MDI"
-      val prisonerNumbers = listOf("A11111A")
+      val prisonerNumbers = listOf("G0459GU")
       val date = LocalDate.now().plusDays(1)
 
       prisonApiMockServer.stubGetScheduledVisitsForPrisonerNumbers(prisonCode, date)
@@ -492,7 +492,7 @@ class ScheduledEventIntegrationTest : IntegrationTestBase() {
     @Sql("classpath:test_data/seed-activity-with-planned-deallocation-date.sql")
     fun `POST - multiple prisoners - scheduled events not returned after planned deallocation date`() {
       val prisonCode = "MDI"
-      val prisonerNumbers = listOf("A11111A")
+      val prisonerNumbers = listOf("G0459GU")
       val date = LocalDate.now().plusDays(2)
 
       prisonApiMockServer.stubGetScheduledVisitsForPrisonerNumbers(prisonCode, date)
--- src/test/resources/test_data/seed-activity-with-planned-deallocation-date.sql
+++ src/test/resources/test_data/seed-activity-with-planned-deallocation-date.sql
@@ -7,11 +7,11 @@ values (1, 1, 'BAS', 'Basic', 1, 125, 150, 1);
 insert into activity_schedule(activity_schedule_id, activity_id, description, internal_location_id, internal_location_code, internal_location_description, capacity, start_date)
 values (1, 1, 'Retirement AM', 1, 'L1', 'Location 1', 10, '2022-10-10');
 
-insert into activity_schedule_slot(activity_schedule_slot_id, activity_schedule_id, start_time, end_time, monday_flag, time_slot)
-values (1, 1, '10:00:00', '11:00:00', true, 'AM');
+insert into activity_schedule_slot(activity_schedule_slot_id, activity_schedule_id, start_time, end_time, monday_flag, tuesday_flag, wednesday_flag, thursday_flag, friday_flag, saturday_flag, sunday_flag, time_slot)
+values (1, 1, '10:00:00', '11:00:00', true, true, true, true, true, true, true, 'AM');
 
 insert into allocation(allocation_id, activity_schedule_id, prisoner_number, booking_id, prison_pay_band_id, start_date, end_date, allocated_time, allocated_by, deallocated_time, deallocated_by, deallocated_reason, suspended_time, suspended_by, suspended_reason, prisoner_status)
-values (1, 1, 'A11111A', 10001, 1, '2022-10-10', null, '2022-10-10 09:00:00', 'MR BLOGS', null, null, null, null, null, null, 'ACTIVE');
+values (1, 1, 'G0459GU', 10001, 1, '2022-10-10', null, '2022-10-10 09:00:00', 'MR BLOGS', null, null, null, null, null, null, 'ACTIVE');
 
 insert into planned_deallocation(planned_deallocation_id, planned_date, planned_by, planned_reason, planned_at, allocation_id)
 values(1, current_date + 1, 'MR BLOGS', 'PLANNED', '2022-10-11 09:00:00', 1);
