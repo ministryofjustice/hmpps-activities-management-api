ddcc3896c5b7d52ce5342a3c92a9a9a3f51b3cb3
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2024-10-18 09:58:36 +0100

    fix integration test for scheduled events

--- src/test/resources/test_data/seed-activity-with-planned-deallocation-date.sql
+++ src/test/resources/test_data/seed-activity-with-planned-deallocation-date.sql
@@ -1,5 +1,5 @@
 insert into activity(activity_id, prison_code, activity_category_id, activity_tier_id, attendance_required, in_cell, piece_work, outside_work, pay_per_session, summary, description, start_date, end_date, risk_level, created_time, created_by, paid)
-values (1, 'MID', 8, 1, true, false, false, false, 'H', 'Retirement', 'Basic retirement', '2022-10-10', null, 'high', '2022-9-21 00:00:00', 'SEED USER', true);
+values (1, 'MID', 1, 1, true, false, false, false, 'H', 'Retirement', 'Basic retirement', '2022-10-10', null, 'high', '2022-9-21 00:00:00', 'SEED USER', true);
 
 insert into activity_pay(activity_pay_id, activity_id, incentive_nomis_code, incentive_level, prison_pay_band_id, rate, piece_rate, piece_rate_items)
 values (1, 1, 'BAS', 'Basic', 1, 125, 150, 1);
@@ -17,4 +17,10 @@ insert into planned_deallocation(planned_deallocation_id, planned_date, planned_
 values(1, current_date + 1, 'MR BLOGS', 'PLANNED', '2022-10-11 09:00:00', 1);
 
 update allocation set planned_deallocation_id = 1 where allocation_id = 1;
+
+insert into scheduled_instance(activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
+values (1, current_date + 1, '10:00:00', '11:00:00', false, null, null, null, null, 'AM');
+
+insert into scheduled_instance(activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
+values (1, current_date + 2, '10:00:00', '11:00:00', false, null, null, null, null, 'AM');
 
