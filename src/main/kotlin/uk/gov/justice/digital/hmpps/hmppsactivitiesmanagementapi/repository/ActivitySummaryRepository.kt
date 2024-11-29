d80a2fb51d33411f4a75d89c1a8b210532369ca2
Lance Munslow <lance.munslow@digital.justice.gov.uk>
2024-11-29 13:47:04 +0000

    SAA-2221: Improve v_activity_summary view performance

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivitySummaryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ActivitySummaryRepository.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ActivitySummary
 
 @Repository
 interface ActivitySummaryRepository : ReadOnlyRepository<ActivitySummary, Long> {
+  @Query("from ActivitySummary s join fetch s.activityCategory where s.prisonCode = :prisonCode")
   fun findAllByPrisonCode(prisonCode: String): List<ActivitySummary>
 }
