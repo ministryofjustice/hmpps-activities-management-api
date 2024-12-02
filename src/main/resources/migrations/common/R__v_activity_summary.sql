5572bed5f4ba78a0c7d1d8a9d6f6c7f36e6fbbaf
Lance Munslow <lance.munslow@digital.justice.gov.uk>
2024-12-02 12:14:21 +0000

    SAA-2221: Improve count of waitlist v_activity_summary view

--- src/main/resources/migrations/common/R__v_activity_summary.sql
+++ src/main/resources/migrations/common/R__v_activity_summary.sql
@@ -5,18 +5,18 @@ select a.activity_id                                           id,
        a.prison_code,
        a.description                                           activity_name,
        max(s.capacity)                                         capacity,
-       count(alloc.allocation_id)                              allocated,
-       (select count(*)
-        from waiting_list w
-        where w.activity_id = a.activity_id
-          and (w.status = 'PENDING' OR w.status = 'APPROVED')) waitlisted,
+       count(distinct alloc.allocation_id)                     allocated,
+       count(distinct w.waiting_list_id)                       waitlisted,
        a.created_time,
        CASE
-           WHEN a.end_date < current_timestamp THEN 'ARCHIVED'
-           ELSE 'LIVE'
+           WHEN a.end_date < current_timestamp THEN
+               'ARCHIVED'
+           ELSE
+               'LIVE'
            END AS                                              activity_state
 from activity a
          join activity_schedule s on a.activity_id = s.activity_id
          left join allocation alloc on s.activity_schedule_id = alloc.activity_schedule_id and alloc.prisoner_status != 'ENDED'
+         left join waiting_list w on w.activity_id = a.activity_id and (w.status::text = 'PENDING'::text or w.status::text = 'APPROVED'::text)
 group by a.activity_id, a.description, a.prison_code
 order by activity_name, a.activity_id;
