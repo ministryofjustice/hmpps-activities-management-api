3472a1657672adf2c5004a772fd0fc9124d530cd
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-12-24 12:38:06 +0000

    SAA-1785 changes to AWS interaction when running locally

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/AWSConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/AWSConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("!test && !local")
+@Profile("!test")
 class AWSConfig() {
   @Bean
   fun s3ClientAnalyticalPlatform(
