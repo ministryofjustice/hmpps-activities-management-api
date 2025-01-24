1aea5fca5ac854e243ec212e22521cc3cb83eb6e
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-01-24 09:35:55 +0000

    Revert "removed start date from sql script to stop duplicate allocations"
    This reverts commit 860083b5d71a6081f4aa188733e34e353d233274.

--- src/main/resources/migrations/common/V2025.01.21__add_unique_constraint_to_allocations.sql
+++ src/main/resources/migrations/common/V2025.01.21__add_unique_constraint_to_allocations.sql
@@ -1,3 +1,3 @@
 ALTER TABLE allocation ADD CONSTRAINT unique_non_ended_prisoner_allocation_key
-    EXCLUDE (activity_schedule_id WITH =, prisoner_number WITH =, prisoner_status WITH =)
+    EXCLUDE (activity_schedule_id WITH =, prisoner_number WITH =, start_date WITH =, prisoner_status WITH =)
     WHERE (prisoner_status != 'ENDED');
