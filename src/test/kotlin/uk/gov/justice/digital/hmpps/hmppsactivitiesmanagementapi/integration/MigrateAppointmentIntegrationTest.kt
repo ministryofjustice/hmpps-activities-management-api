c0cf381154af6d946dc2d499ed8c0f2b0da971de
munslowl <lance.munslow@digital.justice.gov.uk>
2025-02-12 10:24:43 +0000

    SAA-2331: Set appointment start date limit to 5 years

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/MigrateAppointmentIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/MigrateAppointmentIntegrationTest.kt
@@ -171,8 +171,8 @@ class MigrateAppointmentIntegrationTest : AppointmentsIntegrationTestBase() {
   }
 
   @Test
-  fun `migrate appointment rejected if start date is too far into the future and is not a BVLS code`() {
-    val request = appointmentMigrateRequest(categoryCode = "TEST2343", startDate = LocalDate.now().plusDays(371))
+  fun `migrate appointment rejected if start date over 5 years into the future and is not a BVLS category code`() {
+    val request = appointmentMigrateRequest(categoryCode = "TEST2343", startDate = LocalDate.now().plusDays(1785))
 
     prisonerSearchApiMockServer.stubSearchByPrisonerNumbers(
       listOf(request.prisonerNumber!!),
