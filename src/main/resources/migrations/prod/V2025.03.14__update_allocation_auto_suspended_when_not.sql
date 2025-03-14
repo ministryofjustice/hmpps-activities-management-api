f2a7c1c0bf14e70ac3c4669befebaa44a090199e
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2025-03-14 14:27:55 +0000

    SAA-2415: update allocation from auto_suspended to active

--- src/main/resources/migrations/prod/V2025.03.14__update_allocation_auto_suspended_when_not.sql
+++ src/main/resources/migrations/prod/V2025.03.14__update_allocation_auto_suspended_when_not.sql
@@ -1,3 +1,4 @@
-update allocation a set suspended_time = null, suspended_by = null, suspended_reason = null, prisoner_status = 'ACTIVE'
+update allocation a
+set suspended_time = null, suspended_by = null, suspended_reason = null, prisoner_status = 'ACTIVE'
 where a.allocation_id in (173847, 152470) and a.prisoner_status = 'AUTO_SUSPENDED';
 
