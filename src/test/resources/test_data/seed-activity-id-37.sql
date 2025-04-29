c7238403d7087c336c2809e4f32aeb4f34f825b2
munslowl <lance.munslow@digital.justice.gov.uk>
2025-04-29 12:52:59 +0100

    SAA-2414: Record payment status in cancelled scheduled instance

--- src/test/resources/test_data/seed-activity-id-37.sql
+++ src/test/resources/test_data/seed-activity-id-37.sql
@@ -22,26 +22,26 @@ values (3, 2, 'English AM', 1, 'L1', 'Location 1', 10, '2022-10-10');
 insert into activity_schedule_slot(activity_schedule_slot_id, activity_schedule_id, start_time, end_time, monday_flag, time_slot)
 values (3, 3, '14:00:00', '15:00:00', true, 'PM');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (1, 1, now()::date, '10:00:00', '11:00:00', true, now(), 'USER1', 'Location unavailable', null, 'AM');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (1, 1, now()::date, '10:00:00', '11:00:00', true, now(), 'USER1', 'Location unavailable', false, null, 'AM');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (2, 2, '2022-10-10', '14:00:00', '15:00:00', true, now(), 'USER1', 'Location unavailable', null, 'PM');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (2, 2, '2022-10-10', '14:00:00', '15:00:00', true, now(), 'USER1', 'Location unavailable', true, null, 'PM');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (3, 2, now()::date, '14:00:00', '15:00:00', false, null, null, null, null, 'PM');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (3, 2, now()::date, '14:00:00', '15:00:00', false, null, null, null, null, null, 'PM');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (4, 3, current_timestamp + interval '2 day', '14:00:00', '15:00:00', true, now(), 'USER1', 'Location unavailable', null, 'AM');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (4, 3, current_timestamp + interval '2 day', '14:00:00', '15:00:00', true, now(), 'USER1', 'Location unavailable', false, null, 'AM');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (5, 2, current_timestamp + interval '3 day', '17:00:00', '18:00:00', false, null, null, null, null, 'ED');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (5, 2, current_timestamp + interval '3 day', '17:00:00', '18:00:00', false, null, null, null, null, null, 'ED');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (6, 1, current_timestamp + interval '3 day', '17:00:00', '18:00:00', true, now(), 'USER1', 'Location unavailable', null, 'ED');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (6, 1, current_timestamp + interval '3 day', '17:00:00', '18:00:00', true, now(), 'USER1', 'Location unavailable', true, null, 'ED');
 
-insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, comment, time_slot)
-values (7, 1, current_timestamp + interval '1 day', '13:00:00', '14:00:00', true, now(), 'USER1', 'Location unavailable', null, 'PM');
+insert into scheduled_instance(scheduled_instance_id, activity_schedule_id, session_date, start_time, end_time, cancelled, cancelled_time, cancelled_by, cancelled_reason, cancelled_issue_payment, comment, time_slot)
+values (7, 1, current_timestamp + interval '1 day', '13:00:00', '14:00:00', true, now(), 'USER1', 'Location unavailable', false, null, 'PM');
 
 insert into attendance(attendance_id, scheduled_instance_id, prisoner_number, attendance_reason_id, comment, recorded_time, recorded_by, status, pay_amount, bonus_amount, pieces, incentive_level_warning_issued, case_note_id, other_absence_reason, issue_payment)
 values (1, 1, 'A11111A', null, 'comm1', now(), 'USER1', 'COMPLETED', null, 33, 1, true, 1, 'oar1', true);
