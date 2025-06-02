91618cd274ecec845378c7e76d2415e7b0287070
rajaram.nagarajan <rajaram.nagarajan@justice.gov.uk>
2025-06-02 11:04:29 +0100

    SAA:2408 - Added validator for prison payband alias and description

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/PrisonPayBandCreateRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/PrisonPayBandCreateRequest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
 
 @Schema(description = "Describes one instance of a prison pay band")
 data class PrisonPayBandCreateRequest(
@@ -12,10 +13,12 @@ data class PrisonPayBandCreateRequest(
   val displaySequence: Int,
 
   @field:NotEmpty(message = "Alias must be supplied")
+  @field:Size(max = 30, message = "Alias should not exceed {max} characters")
   @Schema(description = "The alternative text to use in place of the description e.g. Low, Medium, High", example = "Low")
   val alias: String,
 
   @field:NotEmpty(message = "Payband description must be supplied")
+  @field:Size(max = 100, message = "Description should not exceed {max} characters")
   @Schema(description = "The description of pay band in this prison", example = "Pay band 1")
   val description: String,
 
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/PrisonPayBandUpdateRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/request/PrisonPayBandUpdateRequest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
 
 @Schema(description = "Describes one instance of a prison pay band")
 data class PrisonPayBandUpdateRequest(
@@ -11,6 +12,7 @@ data class PrisonPayBandUpdateRequest(
   val displaySequence: Int? = null,
 
   @field:NotEmpty(message = "Alias must be supplied")
+  @field:Size(max = 30, message = "Alias should not exceed {max} characters")
   @Schema(description = "The alternative text to use in place of the description e.g. Low, Medium, High", example = "Low")
   val alias: String? = null,
 )
