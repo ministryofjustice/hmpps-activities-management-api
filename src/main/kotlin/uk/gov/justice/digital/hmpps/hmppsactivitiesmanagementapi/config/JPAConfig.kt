2daa3edb50a0b53b20698a589ac98b23979247c4
rajaram.nagarajan <rajaram.nagarajan@justice.gov.uk>
2025-06-10 14:44:52 +0100

    SAA-2420: Recording edits to activity details with envers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/JPAConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/config/JPAConfig.kt
@@ -2,11 +2,13 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.envers.repository.config.EnableEnversRepositories
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJpaAuditing
-@EnableEnversRepositories
-@EnableJpaRepositories(basePackages = ["uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository"])
+@EnableJpaRepositories(
+  repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean::class,
+  basePackages = ["uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.repository"])
 class JPAConfig
