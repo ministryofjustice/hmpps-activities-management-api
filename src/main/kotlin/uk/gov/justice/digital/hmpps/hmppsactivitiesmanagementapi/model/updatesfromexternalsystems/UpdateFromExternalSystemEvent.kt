16ae88ee1efd3776aa8ea9fca66b51bfe6bf95c9
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-06-18 10:13:12 +0100

    HMAI-630 - MarkPrisonerAttendance SQS events listener (#1556)
    * Added SQS listener
    
    * Use model
    
    * Listener tests
    
    * Listener tests
    
    * Make queue service non-private so we can use
    
    * Integration tests
    
    * Added secret to helm
    
    * Added to yml files
    
    * Add new mark prisoner attendance event
    
    * Add new mark prisoner attendance event
    
    * Listener tests
    
    * Integration tests
    
    * Verify actual object
    
    * Format
    
    * Fixed test message test
    
    * Enabled missing feature flags for domain events
    
    * Use never over times(0)
    
    ---------
    
    Co-authored-by: Lance Munslow <lance@munslow.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/updatesfromexternalsystems/UpdateFromExternalSystemEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/updatesfromexternalsystems/UpdateFromExternalSystemEvent.kt
@@ -1,9 +1,22 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.updatesfromexternalsystems
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AttendanceUpdateRequest
+
 data class UpdateFromExternalSystemEvent(
   val messageId: String,
   val eventType: String,
   val description: String? = null,
   val messageAttributes: Map<String, Any?> = emptyMap(),
-  val who: String? = null,
+  val who: String,
+) {
+  fun toMarkPrisonerAttendanceEvent(): MarkPrisonerAttendanceEvent {
+    val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+    return mapper.convertValue(this.messageAttributes, MarkPrisonerAttendanceEvent::class.java)
+  }
+}
+
+data class MarkPrisonerAttendanceEvent(
+  val attendanceUpdateRequests: List<AttendanceUpdateRequest>,
 )
