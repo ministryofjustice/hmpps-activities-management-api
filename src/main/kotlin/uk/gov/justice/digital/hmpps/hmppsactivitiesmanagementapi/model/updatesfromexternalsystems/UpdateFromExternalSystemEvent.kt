a9dea08018489193241021609b5144f17c8c7491
Tom Watkins <tom.watkins@justice.gov.uk>
2025-06-19 10:34:08 +0100

    New method to convert message to deallocation request

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/updatesfromexternalsystems/UpdateFromExternalSystemEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/updatesfromexternalsystems/UpdateFromExternalSystemEvent.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.updatesfromexternalsystems
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.AttendanceUpdateRequest
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.PrisonerDeallocationRequest
 
 data class UpdateFromExternalSystemEvent(
   val messageId: String,
@@ -14,6 +16,11 @@ data class UpdateFromExternalSystemEvent(
   fun toMarkPrisonerAttendanceEvent(): MarkPrisonerAttendanceEvent {
     val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
     return mapper.convertValue(this.messageAttributes, MarkPrisonerAttendanceEvent::class.java)
+  }
+
+  fun toPrisonerDeallocationRequest(): PrisonerDeallocationRequest {
+    val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build()).registerModule(JavaTimeModule())
+    return mapper.convertValue(this.messageAttributes, PrisonerDeallocationRequest::class.java)
   }
 }
 
