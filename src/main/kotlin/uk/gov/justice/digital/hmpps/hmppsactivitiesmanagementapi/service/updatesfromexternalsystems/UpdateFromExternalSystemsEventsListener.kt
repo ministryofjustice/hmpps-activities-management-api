4d56cb92ad467b4ae2985a5cd50cf62054a8bd3b
Tom Watkins <tom.watkins@justice.gov.uk>
2025-06-19 10:59:07 +0100

    Validate request

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/updatesfromexternalsystems/UpdateFromExternalSystemsEventsListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/updatesfromexternalsystems/UpdateFromExternalSystemsEventsListener.kt
@@ -2,6 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.update
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
+import jakarta.validation.Validation
+import jakarta.validation.ValidationException
+import jakarta.validation.Validator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
@@ -20,6 +23,8 @@ class UpdateFromExternalSystemsEventsListener(
   private val attendancesService: AttendancesService,
   private val activityScheduleService: ActivityScheduleService,
 ) {
+  private val validator: Validator = Validation.buildDefaultValidatorFactory().validator
+
   companion object {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
@@ -49,6 +54,10 @@ class UpdateFromExternalSystemsEventsListener(
           caseNote = event.caseNote,
           scheduleInstanceId = event.scheduleInstanceId,
         )
+        val validationIssues = validator.validate(prisonerDeallocationRequest)
+        if (validationIssues.isNotEmpty()) {
+          throw ValidationException("Validation error on ${sqsMessage.eventType}: ${mapper.writeValueAsString(validationIssues)}")
+        }
         activityScheduleService.deallocatePrisoners(event.scheduleId, request = prisonerDeallocationRequest, deallocatedBy = sqsMessage.who)
       }
       else -> {
