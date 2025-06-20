c96dfc66f3d0d97df8080d6498ff4f563d838de7
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-06-20 11:03:20 +0100

    HMAI-629 - Deallocated prisoner from activity event on queue (#1573)
    * New method to convert message to deallocation request
    
    * Changed to use new event data class
    
    * Changed to use new event data class
    
    * Validate request
    
    * Renamed event
    
    * Listener tests
    
    * Format
    
    * Hard code IDs to use without lookup
    
    * Integration tests
    
    * Improved tests
    
    * Added test for validation
    
    * Remove unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/updatesfromexternalsystems/UpdateFromExternalSystemsEventsListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/updatesfromexternalsystems/UpdateFromExternalSystemsEventsListener.kt
@@ -2,11 +2,16 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.update
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
+import jakarta.validation.Validation
+import jakarta.validation.ValidationException
+import jakarta.validation.Validator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.request.PrisonerDeallocationRequest
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.updatesfromexternalsystems.UpdateFromExternalSystemEvent
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.ActivityScheduleService
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service.AttendancesService
 
 const val UPDATE_FROM_EXTERNAL_SYSTEM_QUEUE_NAME = "updatefromexternalsystemevents"
@@ -16,7 +21,10 @@ const val UPDATE_FROM_EXTERNAL_SYSTEM_QUEUE_NAME = "updatefromexternalsystemeven
 class UpdateFromExternalSystemsEventsListener(
   private val mapper: ObjectMapper,
   private val attendancesService: AttendancesService,
+  private val activityScheduleService: ActivityScheduleService,
 ) {
+  private val validator: Validator = Validation.buildDefaultValidatorFactory().validator
+
   companion object {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
@@ -36,6 +44,21 @@ class UpdateFromExternalSystemsEventsListener(
       "MarkPrisonerAttendance" -> {
         val event = sqsMessage.toMarkPrisonerAttendanceEvent()
         attendancesService.mark(principalName = sqsMessage.who, attendances = event.attendanceUpdateRequests)
+      }
+      "DeallocatePrisonerFromActivitySchedule" -> {
+        val event = sqsMessage.toPrisonerDeallocationEvent()
+        val prisonerDeallocationRequest = PrisonerDeallocationRequest(
+          prisonerNumbers = event.prisonerNumbers,
+          reasonCode = event.reasonCode,
+          endDate = event.endDate,
+          caseNote = event.caseNote,
+          scheduleInstanceId = event.scheduleInstanceId,
+        )
+        val validationIssues = validator.validate(prisonerDeallocationRequest)
+        if (validationIssues.isNotEmpty()) {
+          throw ValidationException("Validation error on ${sqsMessage.eventType}: ${validationIssues.joinToString { it.message }}")
+        }
+        activityScheduleService.deallocatePrisoners(event.scheduleId, request = prisonerDeallocationRequest, deallocatedBy = sqsMessage.who)
       }
       else -> {
         log.warn("Unrecognised message type on external system event: ${sqsMessage.eventType}")
