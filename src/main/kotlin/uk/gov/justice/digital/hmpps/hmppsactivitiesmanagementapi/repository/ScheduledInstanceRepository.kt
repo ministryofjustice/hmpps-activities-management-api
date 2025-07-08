e74900e6c97f82f245d1f89c58acd07cfa89f119
Emma Lanigan <98022697+emmalanigan@users.noreply.github.com>
2025-07-08 10:43:13 +0100

    HMAI-625 Get prisoner’s scheduled instances – /prisons/{prisonCode}/{prisonerId}/scheduled-instances (#1608)
    * add function to repository and service
    
    * add failing test; extend scheduled instance fixture to include attendance and advancedAttendance
    
    * add passing service test
    
    * add passing service test
    
    * add data validation to service
    
    * add data validation to service
    
    * add data validation service test
    
    * add controller test
    
    * add controller test
    
    * add failing integration tests
    
    * add failing integration tests
    
    * update repo query; set up new seed file
    
    * amend service to filter out attendances without the correct prisoner number; start integration testing
    
    * amend service to filter out attendances without the correct prisoner number; start integration testing
    
    * add stronger assertion to integration test
    
    * correct service logic
    
    * correct service logic
    
    * add strength to integration test
    
    * strengthen service tests
    
    * strengthen service tests
    
    * Simplified query
    
    * Updated service to use different repository
    
    * Fix unit tests
    
    * Fix unit tests
    
    * Remove cancelled flag
    
    * Adjust integration tests
    
    * Add search to get 0 records
    
    * Use model to make sure dates in correct format
    
    * Use model to make sure dates in correct format
    
    * Add annotations to the ouputted model
    
    ---------
    
    Co-authored-by: Tom Watkins <tom.watkins@justice.gov.uk>
    Co-authored-by: Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ScheduledInstanceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/repository/ScheduledInstanceRepository.kt
@@ -30,6 +30,34 @@ interface ScheduledInstanceRepository : JpaRepository<ScheduledInstance, Long> {
   ): List<ScheduledInstance>
 
   @Query(
+    """
+    SELECT si FROM ScheduledInstance si
+    WHERE si.activitySchedule.activity.prisonCode = :prisonCode
+    AND si.sessionDate >= :startDate
+    AND si.sessionDate <= :endDate
+    AND (:cancelled is null or si.cancelled = :cancelled)
+    AND (:timeSlot is null or si.timeSlot = :timeSlot)
+    AND (
+      EXISTS (
+        SELECT 1 FROM si.attendances a WHERE a.prisonerNumber = :prisonerNumber
+      )
+      OR
+      EXISTS (
+        SELECT 1 FROM si.advanceAttendances aa WHERE aa.prisonerNumber = :prisonerNumber
+      )
+    )
+    """,
+  )
+  fun getActivityScheduleInstancesForPrisonerByPrisonCodeAndDateRange(
+    prisonCode: String,
+    prisonerNumber: String,
+    startDate: LocalDate,
+    endDate: LocalDate,
+    cancelled: Boolean? = null,
+    timeSlot: TimeSlot? = null,
+  ): List<ScheduledInstance>
+
+  @Query(
     "select si " +
       "from ScheduledInstance si " +
       "join fetch si.activitySchedule asch " +
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ScheduledInstanceFixture.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ScheduledInstanceFixture.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.service
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.common.TimeSlot
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Activity
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ActivitySchedule
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.AdvanceAttendance
+import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.Attendance
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.ScheduledInstance
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.refdata.ActivityCategory
 import uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.entity.refdata.EventTier
@@ -61,6 +63,16 @@ object ScheduledInstanceFixture {
       startDate = date,
       scheduleWeeks = 1,
     ),
+    attendances = mutableListOf(
+      AttendancesFixture.instance(),
+      AttendancesFixture.instance(),
+      AttendancesFixture.instance(prisonerNumber = "B1234BB"),
+      AttendancesFixture.instance(prisonerNumber = "C1234CC"),
+    ),
+    advanceAttendances = mutableListOf(
+      AdvanceAttendancesFixture.instance(),
+      AdvanceAttendancesFixture.instance(prisonerNumber = "B1234BB"),
+    ),
     sessionDate = date,
     timeSlot = timeSlot,
     startTime = startTime,
@@ -68,5 +80,112 @@ object ScheduledInstanceFixture {
     cancelled = cancelled,
     cancelledBy = cancelledBy,
     cancelledReason = cancelledReason,
+  )
+}
+
+object AttendancesFixture {
+  fun instance(
+    id: Long = 1,
+    prisonerNumber: String = "A1234AA",
+    date: LocalDate = LocalDate.of(2022, 10, 1),
+    timeSlot: TimeSlot = TimeSlot.PM,
+    startTime: LocalTime = LocalTime.of(12, 0, 0),
+    endTime: LocalTime = LocalTime.of(13, 0, 0),
+    prisonCode: String = "MDI",
+    activityScheduleId: Long = 1000 + id,
+    activityId: Long = 2000 + id,
+    activityCategoryId: Long = 4000 + id,
+    activityCategoryCode: String = "ACTIVITY CATEGORY CODE $activityCategoryId",
+    activityTierId: Long = 6000 + id,
+  ) = Attendance(
+    scheduledInstance = ScheduledInstance(
+      activitySchedule = ActivitySchedule(
+        activity = Activity(
+          prisonCode = prisonCode,
+          activityCategory = ActivityCategory(
+            activityCategoryId,
+            activityCategoryCode,
+            "ACTIVITY CATEGORY NAME  $activityCategoryId",
+            "ACTIVITY CATEGORY DESCRIPTION  $activityCategoryId",
+          ),
+          activityTier = EventTier(
+            activityTierId,
+            "ACTIVITY TIER CODE $activityTierId",
+            "ACTIVITY TIER DESCRIPTION $activityTierId",
+          ),
+          summary = "ACTIVITY SUMMARY $activityId",
+          description = "ACTIVITY DESCRIPTION $activityId",
+          startDate = date,
+          riskLevel = "high",
+          createdTime = LocalDateTime.of(2022, 10, 1, 12, 0, 0),
+          createdBy = "CREATED BY",
+          isPaid = true,
+        ),
+        description = "DESCRIPTION $activityScheduleId",
+        capacity = 10,
+        startDate = date,
+        scheduleWeeks = 1,
+      ),
+      sessionDate = date,
+      startTime = startTime,
+      endTime = endTime,
+      timeSlot = timeSlot,
+    ),
+    prisonerNumber = prisonerNumber,
+  )
+}
+
+object AdvanceAttendancesFixture {
+  fun instance(
+    id: Long = 1,
+    prisonerNumber: String = "A1234AA",
+    date: LocalDate = LocalDate.of(2022, 10, 1),
+    timeSlot: TimeSlot = TimeSlot.PM,
+    startTime: LocalTime = LocalTime.of(12, 0, 0),
+    endTime: LocalTime = LocalTime.of(13, 0, 0),
+    prisonCode: String = "MDI",
+    activityScheduleId: Long = 1000 + id,
+    activityId: Long = 2000 + id,
+    activityCategoryId: Long = 4000 + id,
+    activityCategoryCode: String = "ACTIVITY CATEGORY CODE $activityCategoryId",
+    activityTierId: Long = 6000 + id,
+  ) = AdvanceAttendance(
+    scheduledInstance = ScheduledInstance(
+      activitySchedule = ActivitySchedule(
+        activity = Activity(
+          prisonCode = prisonCode,
+          activityCategory = ActivityCategory(
+            activityCategoryId,
+            activityCategoryCode,
+            "ACTIVITY CATEGORY NAME  $activityCategoryId",
+            "ACTIVITY CATEGORY DESCRIPTION  $activityCategoryId",
+          ),
+          activityTier = EventTier(
+            activityTierId,
+            "ACTIVITY TIER CODE $activityTierId",
+            "ACTIVITY TIER DESCRIPTION $activityTierId",
+          ),
+          summary = "ACTIVITY SUMMARY $activityId",
+          description = "ACTIVITY DESCRIPTION $activityId",
+          startDate = date,
+          riskLevel = "high",
+          createdTime = LocalDateTime.of(2022, 10, 1, 12, 0, 0),
+          createdBy = "CREATED BY",
+          isPaid = true,
+        ),
+        description = "DESCRIPTION $activityScheduleId",
+        capacity = 10,
+        startDate = date,
+        scheduleWeeks = 1,
+      ),
+      sessionDate = date,
+      startTime = startTime,
+      endTime = endTime,
+      timeSlot = timeSlot,
+    ),
+    prisonerNumber = prisonerNumber,
+    issuePayment = true,
+    recordedTime = LocalDateTime.of(2022, 10, 1, 12, 0, 0),
+    recordedBy = "Joe Bloggs",
   )
 }
