d2c738298737cbbc4a1f16dbb2db5082479d7d14
Rajaram Nagarajan <rajaram.nagarajan@justice.gov.uk>
2025-08-19 09:19:22 +0100

    SAA-3517: Removed pay rate history migration api (#1686)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/model/response/PayHistoryMigrateResponse.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi.model.response
-
-data class PayHistoryMigrateResponse(
-  val payRateDataSize: Long,
-  val payHistoryDataSize: Long,
-  val message: String,
-)
