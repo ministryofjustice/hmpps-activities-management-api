24ae2ebdf80a04d9c869ca27836ac273e77dd5aa
munslowl <lance.munslow@digital.justice.gov.uk>
2026-01-14 16:36:24 +0000

    Remove changes to app class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/HmppsActivitiesManagementApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/HmppsActivitiesManagementApi.kt
@@ -1,25 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsactivitiesmanagementapi
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsActivitiesManagementApi {
-  companion object {
-    val log: Logger = LoggerFactory.getLogger(this::class.java)
-  }
+class HmppsActivitiesManagementApi
 
-  fun main(args: Array<String>) {
-    val runtime = Runtime.getRuntime()
-
-    val allocatedMemory = runtime.totalMemory() / 1024 / 1024
-    val maxMemory = runtime.maxMemory() / 1024 / 1024
-
-    log.info("Allocated memory: ${allocatedMemory}MB")
-    log.info("Max memory: ${maxMemory}MB")
-
-    runApplication<HmppsActivitiesManagementApi>(*args)
-  }
+fun main(args: Array<String>) {
+  runApplication<HmppsActivitiesManagementApi>(*args)
 }
