17104b7e26bed8eed416bf93edf81e2eb77e780a
munslowl <lance.munslow@digital.justice.gov.uk>
2026-02-24 16:56:19 +0000

    Remove change

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ScheduledInstanceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/service/ScheduledInstanceService.kt
@@ -84,7 +84,7 @@ class ScheduledInstanceService(
     endDate = dateRange.endInclusive,
     cancelled = cancelled,
     timeSlot = slot,
-  ).toModel(includeAllocations = false)
+  ).toModel()
 
   @Transactional(readOnly = true)
   fun getActivityScheduleInstancesForPrisonerByDateRange(
