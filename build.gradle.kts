173324cf1f6d07c2d39e82f04e5e59d571b30f0f
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2026-02-26 16:44:25 +0000

    SAA-4080 Take ownership of SAR template into A&A service

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
 
   // Test dependencies
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-subject-access-request-test-support:2.0.2")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
