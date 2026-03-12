8b4ec6452f56f583f7ff5f4beceb173114ca0cf6
mitchell-dsilva <mitchell.dsilva@justice.gov.uk>
2026-03-12 12:11:12 +0000

    Added changes for view and audit table

--- src/main/resources/migrations/common/V2026.03.11__update_waitlist_requesters_with_new_option.sql
+++ src/main/resources/migrations/common/V2026.03.11__update_waitlist_requesters_with_new_option.sql
@@ -1,5 +1,26 @@
-ALTER TABLE waiting_list ALTER COLUMN requested_by TYPE varchar(50);
+DROP VIEW IF EXISTS v_sar_waiting_list;
+
+ALTER TABLE waiting_list
+ALTER COLUMN requested_by TYPE varchar(50);
+
+ALTER TABLE waiting_list_aud
+ALTER COLUMN requested_by TYPE varchar(50);
 
 UPDATE waiting_list
 SET requested_by = 'MULTI_DISCIPLINARY_BOARD'
 WHERE requested_by = 'Staff in a sequencing or allocation meeting';
+
+CREATE OR REPLACE VIEW v_sar_waiting_list AS
+SELECT wl.waiting_list_id AS waiting_list_id,
+       wl.prison_code AS prison_code,
+       wl.prisoner_number AS prisoner_number,
+       act.summary AS activity_summary,
+       wl.application_date AS application_date,
+       replace(wl.requested_by, '_', ' ') AS originator,
+       replace(wl.status, '_', ' ') AS status,
+       wl.status_updated_time AS status_date,
+       wl.comments AS comments,
+       wl.creation_time as created_date
+    FROM waiting_list wl
+    JOIN activity act ON act.activity_id = wl.activity_id;
+
