345ea44729ebe9103d59672a35e1b43200c34cf2
munslowl <lance.munslow@digital.justice.gov.uk>
2026-05-07 15:00:31 +0100

    Fix test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ActivityScheduleInstanceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsactivitiesmanagementapi/integration/ActivityScheduleInstanceIntegrationTest.kt
@@ -148,9 +148,7 @@ class ActivityScheduleInstanceIntegrationTest : LocalStackTestBase() {
     @Sql("classpath:test_data/seed-activity-id-1.sql")
     fun `get scheduled attendees by scheduled instance id`() {
       val attendees = webTestClient.getScheduledAttendeesByInstanceId(1)!!
-      assertThat(attendees).hasSize(2)
-      with(attendees[0]) { assertThat(prisonerNumber).isEqualTo("A11111A") }
-      with(attendees[1]) { assertThat(prisonerNumber).isEqualTo("A22222A") }
+      assertThat(attendees).extracting("prisonerNumber").containsExactlyInAnyOrder("A11111A", "A22222A")
     }
 
     @Test
