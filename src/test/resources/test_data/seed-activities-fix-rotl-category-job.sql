54ffea109c6e516a35ce61f56168d57f8d499e36
Ash Gyngell <ashley.gyngell@justice.gov.uk>
2026-08-19 15:07:44 +0100

    int test data fix

--- src/test/resources/test_data/seed-activities-fix-rotl-category-job.sql
+++ src/test/resources/test_data/seed-activities-fix-rotl-category-job.sql
@@ -1,11 +1,10 @@
--- Adds the SAA_ROTL category (which will be added permanently via a future migration) plus two outside work
--- activities that are in an incorrect state and need fixing by the activities-fix-rotl-category job.
--- Each activity has multiple schedules with populated location fields so the job can also prove that all
--- schedule locations are cleared:
+-- Seeds two outside work activities that are in an incorrect state and need fixing by the activities-fix-rotl-category job.
+-- Each activity has multiple schedules with populated internal location details and DPS location IDs so the job can
+-- prove that all schedule locations are cleared:
 --   activity 401 - outside_work is true but the category is still SAA_OTHER
 --   activity 402 - outside_work is true, the category is already SAA_ROTL, but on_wing is incorrectly true
-insert into activity_category(activity_category_id, code, name, description)
-values (10, 'SAA_ROTL', 'Outside activity', 'Temporary absence or ROTL for outside work');
+--   activity schedule 401 & 402 has internal location information.
+--   activity schedule 411 & 412 also has internal location information.
 
 insert into activity(activity_id, prison_code, activity_category_id, activity_tier_id, attendance_required, in_cell, on_wing, off_wing, outside_work, piece_work, pay_per_session, summary, description, start_date, end_date, risk_level, created_time, created_by, paid)
 values (401, 'MDI', 9, 1, true, false, false, false, true, true, 'H', 'Outside work - wrong category', 'Outside work - wrong category', '2022-10-10', null, 'high', '2022-9-21 00:00:00', 'SEED USER', false);
@@ -36,4 +35,3 @@ values (402, 402, '09:00:00', '11:00:00', true, true, true, true, true, 'AM');
 
 insert into activity_schedule_slot(activity_schedule_slot_id, activity_schedule_id, start_time, end_time, monday_flag, tuesday_flag, wednesday_flag, thursday_flag, friday_flag, time_slot)
 values (412, 412, '13:00:00', '15:00:00', true, true, true, true, true, 'PM');
-
